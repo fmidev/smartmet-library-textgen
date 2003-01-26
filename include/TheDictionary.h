1fb0d6b8c38a9ab54b516e452decc52f0de7c407
mheiskan <mheiskan>
2003-01-26 20:54:20 +0000

    TheDictionary on singleton, joka tarjoaa globaalin saatavuuden sanakirjapalveluille. dictionary-metodilla asetetaan haluttu sanakirjatyyppi, luultavasti DictionaryFactory luokkaa apuna käyttäen. Tämän jälkeen init, find, contains, insert jne toimivat kuten normaalisti. Erona on vain, että luokka on singleton, eli käytetään tyyliin   TheDictionary::instance()::init("fi");   TheDictionary::instance()::find("sää"); Eli, sen sijaan että kuljetetaan mukana sanakirja objektia, nyt kutsutaan ylimääräistä instance metodia. Kyseessä on siis normali singleton-ratkaisu. Singleton destruktori-singleton kutsuja ei ole suojattu, mutta niiden tapahtumistodennäköisyys on erittäin pieni, mikä destruktori tarvitsee muka sanakirjaa? Toteutuksessa on käytetty Meyers singletonia, eli ratkaisu ei ole thread-turvallinen.

---
+++ include/TheDictionary.h
@@ -0,0 +1,73 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of the TheDictionary class
+ */
+// ======================================================================
+/*!
+ * \class textgen::TheDictionary
+ *
+ * \brief Provides global dictionary services
+ *
+ * The responsibility of the TheDictionary class is to provide global
+ * access to dictionary services. In essence, TheDictionary is a singleton
+ * as implied by the "The" prefix.
+ *
+ * TheDictionary has a state variable which is the actual dictionary
+ * implementation. The state can be changed by using the dictionary
+ * method. The default dictionary is NullDictionary, meaning the
+ * singleton is unusable before the state has been changed.
+ *
+ * Sample usage:
+ * \code
+ * TheDictionary::instance().dictionary(DictionaryFactory::create("mysql"));
+ * cout << TheDictionary::instance().find("keyword");
+ * \endcode
+ *
+ */
+// ----------------------------------------------------------------------
+
+#ifndef TEXTGEN_THEDICTIONARY_H
+#define TEXTGEN_THEDICTIONARY_H
+
+#include "Dictionary.h"
+#include <memory>
+#include <string>
+
+namespace textgen
+{
+  class TheDictionary
+  {
+  public:
+
+	typedef Dictionary::size_type size_type;
+
+	static TheDictionary & instance();
+
+	void dictionary(std::auto_ptr<Dictionary> theDict);
+
+	void init(const std::string & theLanguage);
+	bool contains(const std::string & theKey) const;
+	const std::string & find(const std::string & theKey) const;
+	void insert(const std::string & theKey, const std::string & thePhrase);
+
+	size_type size(void) const;
+	bool empty(void) const;
+
+  private:
+
+	~TheDictionary();
+	TheDictionary();
+	TheDictionary(const Dictionary & theDict);
+	TheDictionary & operator=(const TheDictionary & theDict);
+
+	static void create();
+	std::auto_ptr<Dictionary> itsDictionary;
+
+  }; // class Dictionary
+
+} // namespace textgen
+
+#endif // TEXTGEN_DICTIONARY_H
+
+// ======================================================================
---
+++ source/TheDictionary.cpp
@@ -0,0 +1,153 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class TheDictionary
+ */
+// ======================================================================
+
+#include "TheDictionary.h"
+
+namespace textgen
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief The void constructor is used privately by instance()
+   */
+  // ----------------------------------------------------------------------
+
+  TheDictionary::TheDictionary()
+	: itsDictionary()
+  { }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief The destructor
+   */
+  // ----------------------------------------------------------------------
+
+  TheDictionary::~TheDictionary()
+  { }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return an instance of TheDictionary
+   *
+   * The instance method is the de factor standard means of getting
+   * access to a singleton.
+   *
+   * \return A reference to the singleton
+   */
+  // ----------------------------------------------------------------------
+
+  TheDictionary & TheDictionary::instance()
+  {
+	static TheDictionary theDict;
+	return theDict;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Set the dictionary state
+   *
+   * Setting the dictionary is effectively setting the dictionary type to
+   * be used. Normally one would use this in association with
+   * DictionaryFactory::create.
+   *
+   * \param The dictionary to take into use
+   */
+  // ----------------------------------------------------------------------
+
+  void TheDictionary::dictionary(std::auto_ptr<Dictionary> theDict)
+  {
+	itsDictionary = theDict;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Initialize with given language
+   *
+   * \param theLanguage The ISO-code of the language
+   */
+  // ----------------------------------------------------------------------
+
+  void TheDictionary::init(const std::string & theLanguage)
+  {
+	itsDictionary->init(theLanguage);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test if the given phrase is in the dictionary
+   *
+   * \param theKey The key of the phrase
+   * \return True if the phrase is in the dictionary.
+   */
+  // ----------------------------------------------------------------------
+
+  bool TheDictionary::contains(const std::string & theKey) const
+  {
+	return itsDictionary->contains(theKey);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return the phrase for the given key.
+   *
+   * This will throw a TextGenError if the phrase is not in
+   * the dictionary.
+   *
+   * \param theKey The key of the phrase
+   * \return The phrase
+   */
+  // ----------------------------------------------------------------------
+
+  const std::string & TheDictionary::find(const std::string & theKey) const
+  {
+	return itsDictionary->find(theKey);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Insert a new phrase into the dictionary
+   *
+   * \param theKey The key of the phrase
+   * \param thePhrase The phrase
+   */
+  // ----------------------------------------------------------------------
+
+  void TheDictionary::insert(const std::string & theKey,
+						  const std::string & thePhrase)
+  {
+	itsDictionary->insert(theKey,thePhrase);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * Return the size of the dictionary
+   *
+   * \return The size of the dictionary
+   */
+  // ----------------------------------------------------------------------
+
+  TheDictionary::size_type TheDictionary::size(void) const
+  {
+	return itsDictionary->size();
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * Test if the dictionary is empty
+   *
+   * \return True if the dictionary is empty
+   */
+  // ----------------------------------------------------------------------
+
+  bool TheDictionary::empty(void) const
+  {
+	return itsDictionary->empty();
+  }
+
+} // namespace textgen
+
+// ----------------------------------------------------------------------
