745d7b714a35a4bf90b99723ab2eb4923e70a40b
mheiskan <mheiskan>
2003-04-04 12:33:00 +0000

    WeatherAnalyzer määrittelee abstraktin interfacen, jonka tekstigeneraattorin käyttämän sääanalysaattorin tulee käyttää. Käytännössä tarvitaan vain yksi metodi, se joka laskee tuloksen. Interfacen toteuttavassa luokassa saattaa sitten olla lisänä erilaisia alustusmetodeja querydatan lisäämiseksi, karttatietojen käsittelemiseksi jne.

---
+++ include/WeatherAnalyzer.h
@@ -0,0 +1,47 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of abstract class WeatherAnalyzer
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::WeatherAnalyzer
+ *
+ * \brief Provides analysis services to clients
+ *
+ * The WeatherAnalyzer class provides an uniform interface for
+ * weather analysis services. The only method required by the
+ * API is the ability to calculate a single result. All the
+ * remaining work is in initializing the class implementing
+ * the abstract interface with query data, map information
+ * and so on.
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_WEATHERANALYZER_H
+#define WEATHERANALYSIS_WEATHERANALYZER_H
+
+#include "WeatherParameter.h"
+#include "WeatherFunction.h"
+
+namespace WeatherAnalysis
+{
+  class WeatherResult;
+  class WeatherPeriod;
+  class WeatherArea;
+
+  class WeatherAnalyzer
+  {
+  public:
+	virtual WeatherResult analyze(const WeatherParameter & theParameter,
+								  const WeatherFunction & theFunction,
+								  const WeatherPeriod & thePeriod,
+								  const WeatherArea & theArea) = 0;
+  }; // class WeatherAnalyzer
+
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_WEATHERANALYZER_H
+
+// ======================================================================
