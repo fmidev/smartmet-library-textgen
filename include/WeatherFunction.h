0ccc55bf6e8ac4b808484e0dcdfa7c97d51745d0
mheiskan <mheiskan>
2003-04-04 06:43:38 +0000

    WeatherFunction on enumeraatio funktioista, joita analysaatoria voidaan pyytää palauttamaan halutulle parametrille. Esimerkiksi minimi, maksimi, keskiarvo, keskihajonta, todennäköisyys.

---
+++ include/WeatherFunction.h
@@ -0,0 +1,45 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Enumeration of WeatherFunction
+ */
+// ======================================================================
+/*!
+ * \enum WeatherAnalysis::WeatherFunction
+ *
+ * \brief Enumeration of functions on parameters that can be analyzed
+ *
+ * This enumeration lists all the functions that can be applied
+ * when analyzing facts from gridded data. For example, one can
+ * calculate the Maximum, Minimum or Mean of the data over space
+ * and time.
+ *
+ * This enumeration is used both as input to the analysis functions
+ * and is stored in WeatherFact along with the results and other
+ * parameters related to a single fact extracted in analysis.
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_WEATHERFUNCTION_H
+#define WEATHERANALYSIS_WEATHERFUNCTION_H
+
+namespace WeatherAnalysis
+{
+
+  enum WeatherFunction
+	{
+	  Maximum,
+	  Minimum,
+	  Mean,
+	  StandardDeviation,
+	  Probability
+
+	}; // enum Weatherfunction
+
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_WEATHERFUNCTION_H
+
+// ======================================================================
+
