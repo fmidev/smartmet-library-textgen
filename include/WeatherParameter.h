b9507f4efffc71e791c8de4c4dd836b7fa5fe05f
mheiskan <mheiskan>
2003-04-04 06:42:34 +0000

    WeatherParameter on enumeraatio sääilmiöistä, joita voidaan analysoida.

---
+++ include/WeatherParameter.h
@@ -0,0 +1,40 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Enumeration of WeatherParameter
+ */
+// ======================================================================
+/*!
+ * \enum WeatherAnalysis::WeatherParameter
+ *
+ * \brief Enumeration of parameters that can be analyzed
+ *
+ * This enumeration is separate from that in newbase library because
+ * we may enumerate phenomena which are not represented separately
+ * in newbase. For example, we list Snow and Sleet as separate parameters
+ * for which one can calculate a Probability.
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_WEATHERPARAMETER_H
+#define WEATHERANALYSIS_WEATHERPARAMETER_H
+
+namespace WeatherAnalysis
+{
+
+  enum WeatherParameter
+	{
+	  Temperature,
+	  Pressure,
+	  WindSpeed,
+	  WindDirection
+
+	}; // enum WeatherParameter
+
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_WEATHERPARAMETER_H
+
+// ======================================================================
+
