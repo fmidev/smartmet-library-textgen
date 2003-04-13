7b6330830872a95f35cc33cdbc8f4ed4795d22c7
mheiskan <mheiskan>
2003-04-13 19:08:15 +0000

    Alustava API grid forecasterille. Se, lukeeko luokka automaattisesti viimeisintä dataa, vai vaaditaanko erillinen Init metodi on vielä ratkaisematta.

---
+++ include/GridForecaster.h
@@ -0,0 +1,50 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class GridForecaster
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::GridForecaster
+ *
+ * \brief Provides forecast analysis services to clients
+ *
+ * The GridForecaster class provides weather forecast analysis
+ * services. All analysis is performed by analyzing the main
+ * grid forecast. Probabilities are calculated by analyzing the
+ * gridded forecasts.
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_GRIDFORECASTER_H
+#define WEATHERANALYSIS_GRIDFORECASTER_H
+
+#include "WeatherForecaster.h"
+
+namespace WeatherAnalysis
+{
+  class WeatherResult;
+  class WeatherPeriod;
+  class WeatherArea;
+
+  class GridForecaster : public WeatherForecaster
+  {
+  public:
+
+	virtual WeatherResult analyze(const WeatherParameter & theParameter,
+								  const WeatherFunction & theFunction,
+								  const WeatherPeriod & thePeriod,
+								  const WeatherArea & theArea) const;
+
+	virtual WeatherResult analyze(const WeatherParameter & theParameter,
+								  const WeatherFunction & theFunction,
+								  const WeatherPeriod & thePeriod,
+								  const NFmiPoint & thePoint) const;
+  }; // class GridForecaster
+
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_GRIDFORECASTER_H
+
+// ======================================================================
