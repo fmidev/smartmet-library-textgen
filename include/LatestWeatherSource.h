72c857759a0b004b6ec2f33a3658e14a45d466ad
mheiskan <mheiskan>
2003-04-14 09:13:50 +0000

    LatestWeatherSource on WeatherSource, joka automaattisesti palauttaa aina viimeisimmän ennustedatan.

---
+++ include/LatestWeatherSource.h
@@ -0,0 +1,45 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class LatestWeatherSource
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::LatestWeatherSource
+ *
+ * \brief Weather data source for the latest forecasts
+ *
+ * The LatestWeatherSource class manages shared instances of weather data.
+ * The class makes sure each requested forecast is the latest possible.
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_LATESTWEATHERSOURCE_H
+#define WEATHERANALYSIS_LATESTWEATHERSOURCE_H
+
+#include "WeatherSource.h"
+#include "boost/shared_ptr.hpp"
+
+namespace WeatherAnalysis
+{
+  class LatestWeatherSource : public WeatherSource
+  {
+  public:
+
+	LatestWeatherSource();
+	virtual boost::shared_ptr<NFmiQueryData> getData(const std::string & theName) const;
+
+  private:
+
+	class Pimple;
+	static boost::shared_ptr<Pimple> itsPimple;
+
+  }; // class LatestWeatherSource
+
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_LATESTWEATHERSOURCE_H
+
+// ======================================================================
+
