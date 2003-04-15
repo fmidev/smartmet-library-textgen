3e905bfbddf9044f7701e898971b66d93672b326
mheiskan <mheiskan>
2003-04-15 11:40:59 +0000

    Lisäsin AnalysisSource luokan, joka tallentaa erilaisia datalähteitä ja cacheja analyysiä varten. Mukana on mm. WeatherSource, MapSource sekä erilaisia MaskSource luokkia.

---
+++ include/AnalysisSources.h
@@ -0,0 +1,54 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class AnalysisSources
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::AnalysisSources
+ *
+ * \brief Contains weather analysis data sources and caches
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_ANALYSISSOURCES_H
+#define WEATHERANALYSIS_ANALYSISSOURCES_H
+
+#include "boost/shared_ptr.hpp"
+
+namespace AnalysisSources
+{
+  class MapSource;
+  class MaskSource;
+  class WeatherSource;
+
+  class AnalysisSources
+  {
+  public:
+
+	boost::shared_ptr<WeatherSource> getWeatherSource() const;
+	boost::shared_ptr<MapSource> getMapSource() const;
+	boost::shared_ptr<MaskSource> getMaskSource() const;
+	boost::shared_ptr<MaskSource> getLandMaskSource() const;
+
+	void setWeatherSource(const boost::shared_ptr<WeatherSource> & theSource);
+	void setMapSource(const boost::shared_ptr<MapSource> & theSource);
+	void setMaskSource(const boost::shared_ptr<MaskSource> & theSource);
+	void setLandMaskSource(const boost::shared_ptr<MaskSource> & theSource);
+
+  private:
+
+	boost::shared_ptr<WeatherSource> itsWeatherSource;
+	boost::shared_ptr<MapSource> itsMapSource;
+	boost::shared_ptr<MaskSource> itsMaskSource;
+	boost::shared_ptr<MaskSource> itsLandMaskSource;
+
+  }; // class AnalysisSources
+
+} // namespace AnalysisSources
+
+#endif // WEATHERANALYSIS_ANALYSISSOURCES_H
+
+// ======================================================================
+
---
+++ source/AnalysisSources.cpp
@@ -0,0 +1,124 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class AnalysisSources
+ */
+// ======================================================================
+
+#include "AnalysisSources.h"
+#include "MapSource.h"
+#include "MaskSource.h"
+#include "WeatherSource.h"
+#include "boost/shared_ptr.hpp"
+
+namespace AnalysisSources
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return the weather source
+   *
+   * \return The weather source
+   */
+  // ----------------------------------------------------------------------
+
+  boost::shared_ptr<WeatherSource> AnalysisSources::getWeatherSource() const
+  {
+	return itsWeatherSource;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return the map source
+   *
+   * \return The map source
+   */
+  // ----------------------------------------------------------------------
+
+  boost::shared_ptr<MapSource> AnalysisSources::getMapSource() const
+  {
+	return itsMapSource;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return the plain mask source
+   *
+   * \return The mask source
+   */
+  // ----------------------------------------------------------------------
+
+  boost::shared_ptr<MaskSource> AnalysisSources::getMaskSource() const
+  {
+	return itsMaskSource;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return the land mask source
+   *
+   * \return The land mask source
+   */
+  // ----------------------------------------------------------------------
+
+  boost::shared_ptr<MaskSource> AnalysisSources::getLandMaskSource() const
+  {
+	return itsLandMaskSource;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Set the weather source
+   *
+   * \param theSource The weather source
+   */
+  // ----------------------------------------------------------------------
+
+  void AnalysisSources::setWeatherSource(const boost::shared_ptr<WeatherSource> & theSource)
+  {
+	itsWeatherSource = theSource;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Set the map source
+   *
+   * \param theSource The map source
+   */
+  // ----------------------------------------------------------------------
+
+  void AnalysisSources::setMapSource(const boost::shared_ptr<MapSource> & theSource)
+  {
+	itsMapSource = theSource;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Set the plain mask source
+   *
+   * \param theSource The mask source
+   */
+  // ----------------------------------------------------------------------
+
+  void AnalysisSources::setMaskSource(const boost::shared_ptr<MaskSource> & theSource)
+  {
+	itsMaskSource = theSource;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Set the land mask source
+   *
+   * \param theSource The land mask source
+   */
+  // ----------------------------------------------------------------------
+
+  void AnalysisSources::setLandMaskSource(const boost::shared_ptr<MaskSource> & theSource)
+  {
+	itsLandMaskSource = theSource;
+  }
+
+} // namespace AnalysisSources
+
+// ======================================================================
+
