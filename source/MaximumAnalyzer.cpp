f016972d009951d22ce1d0df4df7fff87bf3688b
mheiskan <mheiskan>
2003-04-23 10:54:08 +0000

    Siirsin toteutusta TemperatureAnalyzer luokan koodista MaximumAnalyzer luokan koodiin.

--- source/MaximumAnalyzer.cpp
+++ source/MaximumAnalyzer.cpp
@@ -7,9 +7,23 @@
 
 #include "MaximumAnalyzer.h"
 #include "AnalysisSources.h"
+#include "MaskSource.h"
+#include "WeatherAnalysisError.h"
 #include "WeatherArea.h"
 #include "WeatherPeriod.h"
 #include "WeatherResult.h"
+#include "WeatherSource.h"
+
+#include "NFmiEnumConverter.h"
+#include "NFmiFastQueryInfo.h"
+#include "NFmiQueryData.h"
+#include "NFmiSettings.h"
+
+#include "boost/shared_ptr.hpp"
+#include <string>
+
+using namespace std;
+using namespace boost;
 
 namespace WeatherAnalysis
 {
@@ -36,6 +50,43 @@ namespace WeatherAnalysis
 						   const std::string & theDataName,
 						   const std::string & theParameterName) const
   {
+
+	// Establish the data
+
+	if(!NFmiSettings::instance().isset(theDataName))
+	  throw WeatherAnalysisError("No "+theDataName+" defined in settings");
+	string dataname = NFmiSettings::instance().value(theDataName);
+
+	// Get the data into use
+
+	shared_ptr<WeatherSource> wsource = theSources.getWeatherSource();
+	shared_ptr<NFmiQueryData> qd = wsource->data(dataname);
+	NFmiFastQueryInfo qi(qd.get());
+
+	// Try activating the parameter
+
+	NFmiEnumConverter converter;
+	FmiParameterName param = FmiParameterName(converter.ToEnum(theParameterName));
+	if(param == kFmiBadParameter)
+	  throw WeatherAnalysisError("Parameter "+theParameterName+" is not defined in newbase");
+
+	if(!qi.Param(param))
+	  throw WeatherAnalysisError(theParameterName+" is not available in "+dataname);
+	
+	// Handle points and areas separately
+
+	if(theArea.isNamed())
+	  {
+		shared_ptr<MaskSource> msource = theSources.getMaskSource();
+		MaskSource::mask_type mask = msource->mask(theArea,dataname,*wsource);
+
+	  }
+	else
+	  {
+	  }
+	
+
+
 	return WeatherResult(kFloatMissing,0);
   }
 
--- source/TemperatureAnalyzer.cpp
+++ source/TemperatureAnalyzer.cpp
@@ -8,43 +8,10 @@
 #include "TemperatureAnalyzer.h"
 #include "AnalysisSources.h"
 #include "FunctionAnalyzerFactory.h"
-#include "MaskSource.h"
-#include "WeatherAnalysisError.h"
-#include "WeatherArea.h"
-#include "WeatherLimits.h"
-#include "WeatherPeriod.h"
 #include "WeatherResult.h"
-#include "WeatherSource.h"
-
-#include "NFmiDataIntegrator.h"
-#include "NFmiDataModifierMax.h"
-#include "NFmiSettings.h"
-#include "NFmiFastQueryInfo.h"
 
 using namespace std;
 using namespace boost;
-
-namespace
-{
-  // ----------------------------------------------------------------------
-  /*!
-   * \brief Return the temperature forecast source
-   */
-  // ----------------------------------------------------------------------
-
-  string temperature_forecast_source()
-  {
-	using namespace WeatherAnalysis;
-
-	const string varname = "textgen::temperature_forecast";
-	if(!NFmiSettings::instance().isset(varname))
-	  throw WeatherAnalysisError("No temperature source defined in "+varname);
-
-	const string source = NFmiSettings::instance().value(varname);
-	return source;
-  }
-
-}
 
 namespace WeatherAnalysis
 {
@@ -70,23 +37,6 @@ namespace WeatherAnalysis
   {
 	const string varname = "textgen::temperature_forecast";
 	const string parname = "Temperature";
-
-
-	const string dataname = temperature_forecast_source();
-
-	shared_ptr<WeatherSource> wsource = theSources.getWeatherSource();
-	shared_ptr<NFmiQueryData> qd = wsource->data(dataname);
-	NFmiFastQueryInfo qi(qd.get());
-
-	if(!qi.Param(kFmiTemperature))
-	  throw WeatherAnalysisError("Temperature not available in dataname");
-
-	MaskSource::mask_type mask;
-	if(theArea.isNamed())
-	  {
-		shared_ptr<MaskSource> msource = theSources.getMaskSource();
-		mask = msource->mask(theArea,dataname,*wsource);
-	  }
 
 	std::auto_ptr<FunctionAnalyzer> analyzer = FunctionAnalyzerFactory::create(theFunction);
 
