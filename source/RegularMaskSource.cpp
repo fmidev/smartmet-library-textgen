926a9bda803b075aeb6f769fde2d735b814eca7c
mheiskan <mheiskan>
2003-04-28 07:56:13 +0000

    Kirjoitin toteutuksen uusiksi siistimpänä.

--- source/RegularMaskSource.cpp
+++ source/RegularMaskSource.cpp
@@ -12,16 +12,53 @@
 #include "WeatherArea.h"
 #include "WeatherSource.h"
 
+#include "NFmiGrid.h"
+#include "NFmiQueryData.h"
 #include "NFmiIndexMask.h"
 #include "NFmiIndexMaskSource.h"
+#include "NFmiIndexMaskTools.h"
 
 #include <map>
 
 using namespace std;
 using namespace boost;
+using namespace NFmiIndexMaskTools;
+
 
 namespace WeatherAnalysis
 {
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Utility structure to each cache management
+   */
+  // ----------------------------------------------------------------------
+
+  struct WeatherAreaAndID
+  {
+	WeatherId itsID;
+	WeatherArea itsArea;
+
+	WeatherAreaAndID(const WeatherId & theID,
+					 const WeatherArea & theArea)
+	  : itsID(theID)
+	  , itsArea(theArea)
+	{ }
+
+	bool operator==(const WeatherAreaAndID & theOther) const
+	{
+	  return (itsID == theOther.itsID &&
+			  itsArea == theOther.itsArea);
+	}
+
+	bool operator<(const WeatherAreaAndID & theOther) const
+	{
+	  return (itsID != theOther.itsID ?
+			  itsID < theOther.itsID :
+			  itsArea < theOther.itsArea);
+	}
+
+  };
 
   // ----------------------------------------------------------------------
   /*!
@@ -35,16 +72,104 @@ namespace WeatherAnalysis
 	shared_ptr<MapSource> itsMapSource;
 	double itsExpansionDistance;
 
-	typedef map<WeatherArea,mask_type> mask_storage;
-	typedef map<WeatherArea,masks_type> masks_storage;
+	typedef map<WeatherAreaAndID,mask_type> mask_storage;
+	typedef map<WeatherAreaAndID,masks_type> masks_storage;
 
-	typedef map<string,mask_storage> data_mask_storage;
-	typedef map<string,masks_storage> data_masks_storage;
+	mask_storage itsMaskStorage;
+	masks_storage itsMasksStorage;
 
-	data_mask_storage itsMaskStorage;
-	data_masks_storage itsMasksStorage;
+	mask_type find(const WeatherId & theID,
+				   const WeatherArea & theArea) const;
+
+	void insert(const WeatherId & theID,
+				const WeatherArea & theArea,
+				const mask_type & theMask);
+
+	mask_type create_mask(const WeatherArea & theArea,
+						  const std::string & theData,
+						  const WeatherSource & theWeatherSource) const;
 
   }; // class RegularMaskSource::Pimple
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Find mask from cache
+   *
+   * Returns a 0-shared pointer if mask is not found
+   *
+   * \param theID The weather ID
+   * \param theArea The weather area
+   * \return shared_ptr to mask or 0
+   */
+  // ----------------------------------------------------------------------
+
+  RegularMaskSource::mask_type
+  RegularMaskSource::Pimple::find(const WeatherId & theID,
+								  const WeatherArea & theArea) const
+  {
+	static shared_ptr<NFmiIndexMask> dummy;
+
+	WeatherAreaAndID key(theID,theArea);
+
+	mask_storage::const_iterator it = itsMaskStorage.find(key);
+	if(it == itsMaskStorage.end())
+	  return dummy;
+
+	return it->second;
+
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Insert a new mask into the cache
+   *
+   * \param theID The data ID
+   * \param theArea The weather area
+   * \param theMask The mask itself
+   */
+  // ----------------------------------------------------------------------
+
+  void RegularMaskSource::Pimple::insert(const WeatherId & theID,
+										 const WeatherArea & theArea,
+										 const mask_type & theMask)
+  {
+	typedef mask_storage::value_type value_type;
+
+	WeatherAreaAndID key(theID,theArea);
+
+	itsMaskStorage.insert(value_type(key,theMask));
+
+	if(itsMaskStorage.insert(value_type(key,theMask)).second)
+	  throw WeatherAnalysisError("Could not cache mask for "+theArea.name());
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Create a new weather area
+   *
+   * \param theArea The area
+   * \param theWeatherSource The weather source
+   * \return The mask
+   */
+  // ----------------------------------------------------------------------
+
+  RegularMaskSource::mask_type
+  RegularMaskSource::Pimple::create_mask(const WeatherArea & theArea,
+										 const std::string & theData,
+										 const WeatherSource & theWeatherSource) const
+  {
+	const NFmiSvgPath & svg = itsMapSource->getMap(theArea.name());
+
+	shared_ptr<NFmiQueryData> qd = theWeatherSource.data(theData);
+	if(!qd->IsGrid())
+	  throw WeatherAnalysisError("The data in "+theData+" is not gridded - cannot generate mask for it");
+
+	NFmiIndexMask * areamask = new NFmiIndexMask(MaskExpand(qd->GridInfo(),
+															svg,
+															itsExpansionDistance));
+	mask_type sharedmask(areamask);
+	return sharedmask;
+  }
 
   // ----------------------------------------------------------------------
   /*!
@@ -80,31 +205,26 @@ namespace WeatherAnalysis
 						  const std::string & theData,
 						  const WeatherSource & theWeatherSource) const
   {
+	if(!theArea.isNamed())
+	  throw WeatherAnalysisError("Trying to generate mask for point");
+
+	// Establish the ID for the data
+
+	WeatherId id = theWeatherSource.id(theData);
 
 	// Try to find cached mask first
-	Pimple::data_mask_storage::const_iterator it = itsPimple->itsMaskStorage.find(theData);
-	if(it != itsPimple->itsMaskStorage.end())
-	  {
-		Pimple::mask_storage::const_iterator it2 = it->second.find(theArea);
-		if(it2 != it->second.end())
-		  return it2->second;
-	  }
-	else
-	  {
-		typedef Pimple::data_mask_storage::value_type value_type;
-		typedef Pimple::data_mask_storage::const_iterator const_iterator;
 
-		Pimple::mask_storage tmp;
-		pair<const_iterator,bool> result = itsPimple->itsMaskStorage.insert(value_type(theData,tmp));
-		if(result.second)
-		  throw WeatherAnalysisError("Could not allocate room for new mask");
-		it = result.first;
-	  }
+	mask_type areamask = itsPimple->find(id,theArea);
 
-	// Create the mask and store it into container pointed to by it-iterator
+	if(areamask.get()!=0)
+	  return areamask;
 
-	throw WeatherAnalysisError("Unfinished");
+	// Calculate new mask and cache it
 
+	areamask = itsPimple->create_mask(theArea,theData,theWeatherSource);
+	itsPimple->insert(id,theArea,areamask);
+
+	return areamask;
   }
 
   // ----------------------------------------------------------------------
