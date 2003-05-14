6427be61b0f2862f2859d75d59c21e4d4ab9badf
mheiskan <mheiskan>
2003-05-14 11:37:39 +0000

    Lisäsin HtmlTextFormatter luokan toteutuksen.

---
+++ source/HtmlTextFormatter.cpp
@@ -0,0 +1,136 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class HtmlTextFormatter
+ */
+// ======================================================================
+
+#include "HtmlTextFormatter.h"
+#include "Dictionary.h"
+#include "Document.h"
+#include "Glyph.h"
+#include "Header.h"
+#include "Paragraph.h"
+#include "Sentence.h"
+#include "TextFormatterTools.h"
+
+using namespace std;
+using namespace boost;
+
+namespace TextGen
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Set the dictionary to be used while formatting
+   *
+   * \param theDict The dictionary (shared)
+   */
+  // ----------------------------------------------------------------------
+  
+  void HtmlTextFormatter::dictionary(const shared_ptr<Dictionary> & theDict)
+  {
+	itsDictionary = theDict;
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Format a glyph
+   *
+   * \param theGlyph The glyph
+   */
+  // ----------------------------------------------------------------------
+  
+  string HtmlTextFormatter::format(const Glyph & theGlyph) const
+  {
+	return theGlyph.realize(*this);
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Visit a glyph
+   *
+   * \param theGlyph The glyph
+   */
+  // ----------------------------------------------------------------------
+  
+  string HtmlTextFormatter::visit(const Glyph & theGlyph) const
+  {
+	return theGlyph.realize(*itsDictionary);
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Visit a sentence
+   */
+  // ----------------------------------------------------------------------
+  
+  string HtmlTextFormatter::visit(const Sentence & theSentence) const
+  {
+	string ret = TextFormatterTools::realize(theSentence.begin(),
+											 theSentence.end(),
+											 " ",
+											 *this);
+	TextFormatterTools::capitalize(ret);
+	TextFormatterTools::punctuate(ret);
+	
+	return ret;
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Visit a paragraph
+   */
+  // ----------------------------------------------------------------------
+  
+  string HtmlTextFormatter::visit(const Paragraph & theParagraph) const
+  {
+	string ret = "<p>";
+	ret += TextFormatterTools::realize(theParagraph.begin(),
+									   theParagraph.end(),
+									   " ",
+									   *this);
+	ret += "</p>";
+	
+	return ret;
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Visit a header
+   */
+  // ----------------------------------------------------------------------
+  
+  string HtmlTextFormatter::visit(const Header & theHeader) const
+  {
+	string ret = "<h1>";
+	ret +=  TextFormatterTools::realize(theHeader.begin(),
+										theHeader.end(),
+										" ",
+										*this);
+	ret += "</h1>";
+	return ret;
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Visit a document
+   */
+  // ----------------------------------------------------------------------
+  
+  string HtmlTextFormatter::visit(const Document & theDocument) const
+  {
+	string ret = "<div>";
+	ret += TextFormatterTools::realize(theDocument.begin(),
+									   theDocument.end(),
+									   "\n\n",
+									   *this);
+	ret += "</div>";
+	return ret;
+  }
+
+} // namespace TextGen
+  
+// ======================================================================
+  
+  
