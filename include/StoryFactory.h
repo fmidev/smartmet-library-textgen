44d33e13ba5f689e5176d9e7ef29cb4b9f63e8e6
mheiskan <mheiskan>
2003-05-19 12:44:30 +0000

    StoryFactory tuottaa yksittäisiä tekstinkappaleita eri parametreistä. Yleensä nämä kappaleet liitetään jonkin toisen kappaleen perään, kunnes kaikki virkkeet eri parametreista ovat kasassa.

---
+++ include/StoryFactory.h
@@ -0,0 +1,40 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of namespace TextGen::StoryFactory
+ */
+// ======================================================================
+/*!
+ * \namespace TextGen::StoryFactory
+ *
+ * \brief Generates brief paragraphs of desired type
+ */
+// ======================================================================
+
+#ifndef TEXTGEN_STORYFACTORY_H
+#define TEXTGEN_STORYFACTORY_H
+
+#include <string>
+
+namespace WeatherAnalysis
+{
+  class AnalysisSources;
+  class WeatherArea;
+}
+
+namespace TextGen
+{
+  class Paragraph;
+
+  namespace StoryFactory
+  {
+	Paragraph create(const WeatherAnalysis::AnalysisSources & theSources,
+					 const WeatherAnalysis::WeatherArea & theArea,
+					 const std::string & theName);
+
+  } // namespace StoryFactory
+} // namespace TextGen
+
+#endif // TEXTGEN_STORYFACTORY_H
+
+// ======================================================================
---
+++ source/StoryFactory.cpp
@@ -0,0 +1,41 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of namespace TextGen::StoryFactory
+ */
+// ======================================================================
+
+#include "StoryFactory.h"
+#include "AnalysisSources.h"
+#include "Paragraph.h"
+#include "TextGenError.h"
+
+using namespace WeatherAnalysis;
+using namespace std;
+
+namespace TextGen
+{
+  namespace StoryFactory
+  {
+	
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Create a story on the desired subject
+	 *
+	 * Throws if the given name is not recognized.
+	 *
+	 * \param theSources The associated analysis sources
+	 * \param theName The story to create
+	 */
+	// ----------------------------------------------------------------------
+
+	Paragraph create(const AnalysisSources & theSources,
+					 const string & theName)
+	{
+	  throw TextGenError("StoryFactory: Unrecognized story "+theName);
+	}
+
+  } // namespace StoryFactory
+} // namespace TextGen
+
+// ======================================================================
