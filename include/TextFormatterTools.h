ff4b1fd83a6b6ca81e6631c904473b10c7f4aa73
mheiskan <mheiskan>
2003-05-20 11:34:38 +0000

    Lisäsin dokumentaatiota namespacen tarkoituksesta.

--- include/TextFormatterTools.h
+++ include/TextFormatterTools.h
@@ -4,6 +4,13 @@
  * \brief Interface of namespace TextFormatterTools
  */
 // ======================================================================
+/*!
+ * \namespace TextGen::TextFormatterTools
+ *
+ * \brief Common utility functions for actual TextFormatter classes
+ *
+ */
+// ======================================================================
 
 #ifndef TEXTGEN_TEXTFORMATTERTOOLS_H
 #define TEXTGEN_TEXTFORMATTERTOOLS_H
