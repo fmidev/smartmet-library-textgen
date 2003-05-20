8dd5383dd0b87b8e1c4e898d8522c56faff4bf6a
mheiskan <mheiskan>
2003-05-20 04:47:40 +0000

    Alustavat toteutukset, poislukien itse tarinankertojametodit.

---
+++ source/PrecipitationStory.cpp
@@ -0,0 +1,83 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class PrecipitationStory
+ */
+// ======================================================================
+
+#include "PrecipitationStory.h"
+#include "Paragraph.h"
+#include "TextGenError.h"
+
+using namespace WeatherAnalysis;
+using namespace std;
+
+namespace TextGen
+{
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Destructor
+   */
+  // ----------------------------------------------------------------------
+  
+  PrecipitationStory::~PrecipitationStory()
+  {
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Constructor
+   *
+   * \param theSources The analysis sources
+   * \param theArea The area to be analyzed
+   */
+  // ----------------------------------------------------------------------
+  
+  PrecipitationStory::PrecipitationStory(const AnalysisSources & theSources,
+									 const WeatherArea & theArea)
+	: itsSources(theSources)
+	, itsArea(theArea)
+  {
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test whether the given story is known to this class
+   *
+   * \param theName The story name
+   * \return True if this class can generate the story
+   */
+  // ----------------------------------------------------------------------
+  
+  bool PrecipitationStory::hasStory(const string & theName)
+  {
+	if(theName == "precipitation_total")
+	  return true;
+	return false;
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Generate the desired story
+   *
+   * Throws if the story name is not recognized.
+   *
+   * \param theName The story name
+   * \return Paragraph containing the story
+   */
+  // ----------------------------------------------------------------------.
+  
+  Paragraph PrecipitationStory::makeStory(const string & theName) const
+  {
+	if(theName == "precipitation_total")
+	  return total();
+
+	throw TextGenError("PrecipitationStory cannot make story "+theName);
+
+  }
+
+
+} // namespace TextGen
+  
+// ======================================================================
+  
---
+++ source/TemperatureStory.cpp
@@ -0,0 +1,91 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class TemperatureStory
+ */
+// ======================================================================
+
+#include "TemperatureStory.h"
+#include "Paragraph.h"
+#include "TextGenError.h"
+
+using namespace WeatherAnalysis;
+using namespace std;
+
+namespace TextGen
+{
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Destructor
+   */
+  // ----------------------------------------------------------------------
+  
+  TemperatureStory::~TemperatureStory()
+  {
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Constructor
+   *
+   * \param theSources The analysis sources
+   * \param theArea The area to be analyzed
+   */
+  // ----------------------------------------------------------------------
+  
+  TemperatureStory::TemperatureStory(const AnalysisSources & theSources,
+									 const WeatherArea & theArea)
+	: itsSources(theSources)
+	, itsArea(theArea)
+  {
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test whether the given story is known to this class
+   *
+   * \param theName The story name
+   * \return True if this class can generate the story
+   */
+  // ----------------------------------------------------------------------
+  
+  bool TemperatureStory::hasStory(const string & theName)
+  {
+	if(theName == "temperature_mean")
+	  return true;
+	if(theName == "temperature_meanmax")
+	  return true;
+	if(theName == "temperature_meanmin")
+	  return true;
+	return false;
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Generate the desired story
+   *
+   * Throws if the story name is not recognized.
+   *
+   * \param theName The story name
+   * \return Paragraph containing the story
+   */
+  // ----------------------------------------------------------------------.
+  
+  Paragraph TemperatureStory::makeStory(const string & theName) const
+  {
+	if(theName == "temperature_mean")
+	  return mean();
+	if(theName == "temperature_meanmax")
+	  return meanmax();
+	if(theName == "temperature_meanmin")
+	  return meanmin();
+
+	throw TextGenError("TemperatureStory cannot make story "+theName);
+
+  }
+
+
+} // namespace TextGen
+  
+// ======================================================================
+  
