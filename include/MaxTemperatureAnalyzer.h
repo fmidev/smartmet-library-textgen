c2e7f399349714df9e0bd0d5f8c60ed7a5e36518
mheiskan <mheiskan>
2003-05-21 05:47:17 +0000

    Lisäsin MaxTemperature ja MinTemperature parametrien analysaattorien interfacet.

---
+++ include/MaxTemperatureAnalyzer.h
@@ -0,0 +1,43 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class WeatherAnalysis::MaxTemperatureAnalyzer
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::MaxTemperatureAnalyzer
+ *
+ * \brief MaxTemperature analysis functions
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYZER_MAXTEMPERATUREANALYZER_H
+#define WEATHERANALYZER_MAXTEMPERATUREANALYZER_H
+
+#include "WeatherFunction.h"
+#include "ParameterAnalyzer.h"
+
+namespace WeatherAnalysis
+{
+  class AnalyzerSources;
+  class WeatherArea;
+  class WeatherLimits;
+  class WeatherPeriod;
+  class WeatherResult;
+
+  class MaxTemperatureAnalyzer : public ParameterAnalyzer
+  {
+  public:
+
+	virtual WeatherResult analyze(const AnalysisSources & theSources,
+								  const WeatherFunction & theFunction,
+								  const WeatherLimits & theLimits,
+								  const WeatherPeriod & thePeriod,
+								  const WeatherArea & theArea) const;
+  };
+}
+
+#endif // WEATHERANALYSIS_MAXTEMPERATUREANALYZER_H
+
+// ======================================================================
---
+++ include/MinTemperatureAnalyzer.h
@@ -0,0 +1,43 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class WeatherAnalysis::MinTemperatureAnalyzer
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::MinTemperatureAnalyzer
+ *
+ * \brief MinTemperature analysis functions
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYZER_MINTEMPERATUREANALYZER_H
+#define WEATHERANALYZER_MINTEMPERATUREANALYZER_H
+
+#include "WeatherFunction.h"
+#include "ParameterAnalyzer.h"
+
+namespace WeatherAnalysis
+{
+  class AnalyzerSources;
+  class WeatherArea;
+  class WeatherLimits;
+  class WeatherPeriod;
+  class WeatherResult;
+
+  class MinTemperatureAnalyzer : public ParameterAnalyzer
+  {
+  public:
+
+	virtual WeatherResult analyze(const AnalysisSources & theSources,
+								  const WeatherFunction & theFunction,
+								  const WeatherLimits & theLimits,
+								  const WeatherPeriod & thePeriod,
+								  const WeatherArea & theArea) const;
+  };
+}
+
+#endif // WEATHERANALYSIS_MINTEMPERATUREANALYZER_H
+
+// ======================================================================
