e7891c285300c7a4b20c1d11af44e7b9a26abc98
mheiskan <mheiskan>
2003-05-21 05:37:31 +0000

    Toteutin MeanAnalyzer luokan, aiemmin palautti vain puuttuvia arvoja.

--- source/MeanAnalyzer.cpp
+++ source/MeanAnalyzer.cpp
@@ -7,16 +7,32 @@
 
 #include "MeanAnalyzer.h"
 #include "AnalysisSources.h"
+#include "MaskSource.h"
+#include "WeatherAnalysisError.h"
 #include "WeatherArea.h"
 #include "WeatherPeriod.h"
 #include "WeatherResult.h"
+#include "WeatherSource.h"
+
+#include "NFmiDataIntegrator.h"
+#include "NFmiDataModifierAvg.h"
+#include "NFmiEnumConverter.h"
+#include "NFmiFastQueryInfo.h"
+#include "NFmiQueryData.h"
+#include "NFmiSettings.h"
+
+#include "boost/shared_ptr.hpp"
+#include <string>
+
+using namespace std;
+using namespace boost;
 
 namespace WeatherAnalysis
 {
 
   // ----------------------------------------------------------------------
   /*!
-   * \brief Analyze parameter mean value
+   * \brief Analyze parameter area mean of time means
    *
    * \param theSources Analysis sources
    * \param theLimits Analysis limits, not used
@@ -30,12 +46,75 @@ namespace WeatherAnalysis
 
   WeatherResult
   MeanAnalyzer::analyze(const AnalysisSources & theSources,
-						   const WeatherLimits & theLimits,
-						   const WeatherPeriod & thePeriod,
-						   const WeatherArea & theArea,
-						   const std::string & theDataName,
-						   const std::string & theParameterName) const
+						const WeatherLimits & theLimits,
+						const WeatherPeriod & thePeriod,
+						const WeatherArea & theArea,
+						const std::string & theDataName,
+						const std::string & theParameterName) const
   {
+
+	// Establish the data
+
+	if(!NFmiSettings::instance().isset(theDataName))
+	  throw WeatherAnalysisError("No "+theDataName+" defined in settings");
+	string dataname = NFmiSettings::instance().value(theDataName);
+
+	// Get the data into use
+
+	shared_ptr<WeatherSource> wsource = theSources.getWeatherSource();
+	shared_ptr<NFmiQueryData> qd = wsource->data(dataname);
+	NFmiFastQueryInfo qi(qd.get());
+
+	// Try activating the parameter
+
+	NFmiEnumConverter converter;
+	FmiParameterName param = FmiParameterName(converter.ToEnum(theParameterName));
+	if(param == kFmiBadParameter)
+	  throw WeatherAnalysisError("Parameter "+theParameterName+" is not defined in newbase");
+
+	if(!qi.Param(param))
+	  throw WeatherAnalysisError(theParameterName+" is not available in "+dataname);
+	
+	// Handle points and areas separately
+
+	if(theArea.isNamed())
+	  {
+		shared_ptr<MaskSource> msource = theSources.getMaskSource();
+		MaskSource::mask_type mask = msource->mask(theArea,dataname,*wsource);
+
+		// Result
+
+		NFmiDataModifierAvg spacemodifier;
+		NFmiDataModifierAvg timemodifier;
+
+		float result = NFmiDataIntegrator::Integrate(qi,
+													 thePeriod.utcStartTime(),
+													 thePeriod.utcEndTime(),
+													 timemodifier,
+													 *mask,
+													 spacemodifier);
+
+		if(result == kFloatMissing)
+		  return WeatherResult(kFloatMissing,0);
+
+		return WeatherResult(result,1);
+
+	  }
+	else
+	  {
+		if(!(qi.Location(theArea.point())))
+		  throw WeatherAnalysisError("Could not set desired coordinate in "+dataname);
+
+		NFmiDataModifierAvg timemodifier;
+		float result = NFmiDataIntegrator::Integrate(qi,
+													 thePeriod.utcStartTime(),
+													 thePeriod.utcEndTime(),
+													 timemodifier);
+
+		return WeatherResult(result,1);
+
+	  }
+
 	return WeatherResult(kFloatMissing,0);
   }
 
