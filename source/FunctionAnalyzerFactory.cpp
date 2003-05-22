993edc8487365563320f6d32ba3f911a9f795d8c
mheiskan <mheiskan>
2003-05-22 05:47:32 +0000

    FunctionAnalyzerFactory tukee nyt uusia MeanSum, MinSum ja MaxSum funktioita.

--- source/FunctionAnalyzerFactory.cpp
+++ source/FunctionAnalyzerFactory.cpp
@@ -6,13 +6,16 @@
 // ======================================================================
 
 #include "FunctionAnalyzerFactory.h"
-#include "MeanAnalyzer.h"
 #include "MaxMaximumAnalyzer.h"
-#include "MeanMaximumAnalyzer.h"
-#include "MinMaximumAnalyzer.h"
 #include "MaxMinimumAnalyzer.h"
+#include "MaxSumAnalyzer.h"
+#include "MeanAnalyzer.h"
+#include "MeanMaximumAnalyzer.h"
 #include "MeanMinimumAnalyzer.h"
+#include "MeanSumAnalyzer.h"
+#include "MinMaximumAnalyzer.h"
 #include "MinMinimumAnalyzer.h"
+#include "MinSumAnalyzer.h"
 #include "WeatherAnalysisError.h"
 
 namespace WeatherAnalysis
@@ -44,6 +47,12 @@ namespace WeatherAnalysis
 		  return boost::shared_ptr<FunctionAnalyzer>(new MeanMinimumAnalyzer);
 		case MinMinimum:
 		  return boost::shared_ptr<FunctionAnalyzer>(new MinMinimumAnalyzer);
+		case MeanSum:
+		  return boost::shared_ptr<FunctionAnalyzer>(new MeanSumAnalyzer);
+		case MaxSum:
+		  return boost::shared_ptr<FunctionAnalyzer>(new MaxSumAnalyzer);
+		case MinSum:
+		  return boost::shared_ptr<FunctionAnalyzer>(new MinSumAnalyzer);
 		}
 	  throw WeatherAnalysisError("FunctionAnalyzerFactory does not support requested function");
 	}
