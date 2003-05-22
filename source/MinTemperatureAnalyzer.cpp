02fc2344b8a5f52a2ee7121cf5d9ab875427af99
mheiskan <mheiskan>
2003-05-22 05:32:05 +0000

    Lisäsin MaxTemperature ja MinTemperature parametrien analysaattorit. Tuli vasta nyt mahdolliseksi, kun FunctionAnalyzer tukee 3D integraaleja, samoin kuin newbasen NFmiDataIntegrator.

---
+++ source/MinTemperatureAnalyzer.cpp
@@ -0,0 +1,56 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class WeatherAnalysis::MinTemperatureAnalyzer
+ */
+// ======================================================================
+
+#include "MinTemperatureAnalyzer.h"
+#include "AnalysisSources.h"
+#include "FunctionAnalyzerFactory.h"
+#include "WeatherResult.h"
+
+#include "NFmiDataModifierMin.h"
+
+using namespace std;
+using namespace boost;
+
+namespace WeatherAnalysis
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Analyze daily maximum temperature in an area
+   *
+   * \param theSources Analysis sources
+   * \param theFunction The function to analyze
+   * \param theLimits The optional limits for the function
+   * \param thePeriod The time period
+   * \param theArea The area
+   */
+  // ----------------------------------------------------------------------
+  
+  WeatherResult
+  MinTemperatureAnalyzer::analyze(const AnalysisSources & theSources,
+								  const WeatherFunction & theFunction,
+								  const WeatherLimits & theLimits,
+								  const WeatherPeriod & thePeriod,
+								  const WeatherArea & theArea) const
+  {
+	const string varname = "textgen::temperature_forecast";
+	const string parname = "Temperature";
+
+	shared_ptr<FunctionAnalyzer> analyzer = FunctionAnalyzerFactory::create(theFunction);
+
+	NFmiDataModifierMin modifier;
+
+	const int one_day = 24;
+	return analyzer->analyze(theSources,theLimits,thePeriod,theArea,
+							 varname,parname,
+							 one_day,modifier);
+
+  }
+
+} // namespace WeatherAnalysis
+
+// ======================================================================
