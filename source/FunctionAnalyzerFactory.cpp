8532ffc22d20e054a592fece57aee001d6270805
mheiskan <mheiskan>
2003-05-23 11:35:46 +0000

    FunctionAnalyzerFactory redusoitui pieneksi poistojen myötä.

--- source/FunctionAnalyzerFactory.cpp
+++ source/FunctionAnalyzerFactory.cpp
@@ -6,16 +6,6 @@
 // ======================================================================
 
 #include "FunctionAnalyzerFactory.h"
-#include "MaxMaximumAnalyzer.h"
-#include "MaxMinimumAnalyzer.h"
-#include "MaxSumAnalyzer.h"
-#include "MeanAnalyzer.h"
-#include "MeanMaximumAnalyzer.h"
-#include "MeanMinimumAnalyzer.h"
-#include "MeanSumAnalyzer.h"
-#include "MinMaximumAnalyzer.h"
-#include "MinMinimumAnalyzer.h"
-#include "MinSumAnalyzer.h"
 #include "WeatherAnalysisError.h"
 
 namespace WeatherAnalysis
@@ -33,27 +23,6 @@ namespace WeatherAnalysis
 	{
 	  switch(theFunction)
 		{
-		case Mean:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MeanAnalyzer);
-		case MaxMaximum:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MaxMaximumAnalyzer);
-		case MeanMaximum:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MeanMaximumAnalyzer);
-		case MinMaximum:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MinMaximumAnalyzer);
-		case MaxMinimum:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MaxMinimumAnalyzer);
-		case MeanMinimum:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MeanMinimumAnalyzer);
-		case MinMinimum:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MinMinimumAnalyzer);
-
-		case MeanSum:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MeanSumAnalyzer);
-		case MaxSum:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MaxSumAnalyzer);
-		case MinSum:
-		  return boost::shared_ptr<FunctionAnalyzer>(new MinSumAnalyzer);
 
 		}
 	  throw WeatherAnalysisError("FunctionAnalyzerFactory does not support requested function");
