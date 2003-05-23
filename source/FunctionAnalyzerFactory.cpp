8a2316eec16626e8109bd345c4954f465250541e
mheiskan <mheiskan>
2003-05-23 09:36:08 +0000

    Siistin hieman koodia.

--- source/FunctionAnalyzerFactory.cpp
+++ source/FunctionAnalyzerFactory.cpp
@@ -47,12 +47,14 @@ namespace WeatherAnalysis
 		  return boost::shared_ptr<FunctionAnalyzer>(new MeanMinimumAnalyzer);
 		case MinMinimum:
 		  return boost::shared_ptr<FunctionAnalyzer>(new MinMinimumAnalyzer);
+
 		case MeanSum:
 		  return boost::shared_ptr<FunctionAnalyzer>(new MeanSumAnalyzer);
 		case MaxSum:
 		  return boost::shared_ptr<FunctionAnalyzer>(new MaxSumAnalyzer);
 		case MinSum:
 		  return boost::shared_ptr<FunctionAnalyzer>(new MinSumAnalyzer);
+
 		}
 	  throw WeatherAnalysisError("FunctionAnalyzerFactory does not support requested function");
 	}
