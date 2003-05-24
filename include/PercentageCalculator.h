a9e86b59e0c593312f85c2a8340548d59c8f40ec
mheiskan <mheiskan>
2003-05-24 13:31:35 +0000

    PercentageCalculator laskee annetun predikaatin onnistumisprosentin.

---
+++ include/PercentageCalculator.h
@@ -0,0 +1,50 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class WeatherAnalysis::PercentageCalculator
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::PercentageCalculator
+ *
+ * \brief Percentage intergator
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_PERCENTAGECALCULATOR_H
+#define WEATHERANALYSIS_PERCENTAGECALCULATOR_H
+
+#include "Calculator.h"
+#include "boost/shared_ptr.hpp"
+
+namespace WeatherAnalysis
+{
+  class PercentageCalculator : public Calculator
+  {
+  public:
+
+	PercentageCalculator();
+	virtual ~PercentageCalculator() { }
+	virtual void operator()(float theValue);
+	virtual float operator()() const;
+	virtual void acceptor(const Acceptor & theAcceptor);
+	virtual boost::shared_ptr<Calculator> clone() const;
+	virtual void reset();
+
+	void condition(const Acceptor & theCondition);
+
+  private:
+
+	boost::shared_ptr<Acceptor> itsAcceptor;
+	boost::shared_ptr<Acceptor> itsCondition;
+	long itsCounter;
+	long itsTotalCounter;
+
+  }; // class PercentageCalculator
+
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_PERCENTAGECALCULATOR_H
+
+// ======================================================================
---
+++ source/PercentageCalculator.cpp
@@ -0,0 +1,118 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class WeatherAnalysis::PercentageCalculator
+ */
+// ======================================================================
+
+#include "PercentageCalculator.h"
+#include "DefaultAcceptor.h"
+#include "NullAcceptor.h"
+
+#include "NFmiGlobals.h"
+
+using namespace boost;
+
+namespace WeatherAnalysis
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Constructor
+   */
+  // ----------------------------------------------------------------------
+
+  PercentageCalculator::PercentageCalculator()
+	: itsAcceptor(new DefaultAcceptor())
+	, itsCondition(new NullAcceptor())
+	, itsCounter(0)
+	, itsTotalCounter(0)
+  {
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Integrate a new value
+   *
+   * \param theValue
+   */
+  // ----------------------------------------------------------------------
+
+  void PercentageCalculator::operator()(float theValue)
+  {
+	if(itsAcceptor->accept(theValue))
+	  {
+		++itsTotalCounter;
+		if(itsCondition->accept(theValue))
+		  ++itsCounter;
+	  }
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return the integrated value
+   *
+   * \return The integrated percentage value in the range 0-100
+   */
+  // ----------------------------------------------------------------------
+
+  float PercentageCalculator::operator()() const
+  {
+	if(itsTotalCounter==0)
+	  return kFloatMissing;
+	else
+	  return 100*static_cast<float>(itsCounter)/itsTotalCounter;
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Set the internal acceptor
+   *
+   * \param theAcceptor The acceptor to be used
+   */
+  // ----------------------------------------------------------------------
+
+  void PercentageCalculator::acceptor(const Acceptor & theAcceptor)
+  {
+	itsAcceptor = shared_ptr<Acceptor>(theAcceptor.clone());
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Set the actual percentage calculator condition
+   *
+   * \param theCondition The condition to be used
+   */
+  // ----------------------------------------------------------------------
+
+  void PercentageCalculator::condition(const Acceptor & theCondition)
+  {
+	itsCondition = shared_ptr<Acceptor>(theCondition.clone());
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Clone
+   */
+  // ----------------------------------------------------------------------
+
+  boost::shared_ptr<Calculator> PercentageCalculator::clone() const
+  {
+	return boost::shared_ptr<Calculator>(new PercentageCalculator(*this));
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Reset
+   */
+  // ----------------------------------------------------------------------
+
+  void PercentageCalculator::reset()
+  {
+	itsCounter = 0;
+	itsTotalCounter = 0;
+  }
+
+} // namespace WeatherAnalysis
+
+// ======================================================================
