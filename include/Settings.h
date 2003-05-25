b10f49f3161e2c14906ea422871833bd514b01f0
mheiskan <mheiskan>
2003-05-25 15:42:04 +0000

    Settings namespace tarjoaa funktioita NFmiSettings asetusten edistyneempään käsittelyyn. Mukana ovat esimerkiksi bool isset(variable); string require(variable); string require_string(variable); (ei tyhjä!) int require_int(variable); double require_double(variable); int require_hour(variable); (int 0-23) int require_days(variable); (int >= 0)
    Näin kaikki parserointi ja virheentarkistus saadaan keskitettyä
    yhteen moduuliin. Erityisesti require_hour ja require_days
    eivät olisi soveltuneet NFmiSettings luokan metodeiksi, joten
    sen laajennus ei tullut kyseeseen.
    
    Namespace on oma, ei upotettu TextGen eikä WeatherAnalysis
    namespaceen, koska kumpikin namespace käyttää Settings
    namespacea.

---
+++ include/Settings.h
@@ -0,0 +1,35 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of namespace Settings
+ */
+// ----------------------------------------------------------------------
+/*!
+ * \namespace Settings
+ *
+ * \brief Provides parsed and checked access to NFmiSettings
+ *
+ */
+// ----------------------------------------------------------------------
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <string>
+
+namespace Settings
+{
+  bool isset(const std::string & theName);
+  std::string require(const std::string & theName);
+  std::string require_string(const std::string & theName);
+  int require_int(const std::string & theName);
+  double require_double(const std::string & theName);
+  int require_hour(const std::string & theName);
+  int require_days(const std::string & theName);
+
+} // namespace Settings
+
+#endif // SETTINGS_H
+
+// ======================================================================
+
---
+++ source/Settings.cpp
@@ -0,0 +1,162 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of namespace Settings
+ */
+// ======================================================================
+
+#include "Settings.h"
+#include "NFmiSettings.h"
+
+#include "boost/lexical_cast.hpp"
+
+#include <stdexcept>
+
+using namespace std;
+using namespace boost;
+
+namespace Settings
+{
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test whether the given variable is set
+   *
+   * \param theName The variable name
+   * \return True if the variable is set
+   */
+  // ----------------------------------------------------------------------
+  
+  bool isset(const std::string & theName)
+  {
+	return NFmiSettings::instance().isset(theName);
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Require the string value of the given variable
+   *
+   * Throws if the variable is not set
+   *
+   * \param theName The variable name
+   * \return The value of the variable
+   */
+  // ----------------------------------------------------------------------
+  
+  std::string require(const std::string & theName)
+  {
+	if(!NFmiSettings::instance().isset(theName))
+	  throw runtime_error("The variable "+theName+" is required");
+	return NFmiSettings::instance().value(theName);
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Require the nonempty string value of the given variable
+   *
+   * Throws if the variable is not set or if the value is empty
+   *
+   * \param theName The variable name
+   * \return The integer value
+   */
+  // ----------------------------------------------------------------------
+  
+  std::string require_string(const std::string & theName)
+  {
+	const string value = require(theName);
+	if(value.empty())
+	  throw runtime_error("Value of "+theName+" must be nonempty");
+	return value;
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Require the integer value of the given variable
+   *
+   * Throws if the variable is not set or if the value is not an integer.
+   *
+   * \param theName The variable name
+   * \return The integer value
+   */
+  // ----------------------------------------------------------------------
+  
+  int require_int(const std::string & theName)
+  {
+	const string value = require(theName);
+	try
+	  {
+		int val = lexical_cast<int>(value);
+		return val;
+	  }
+	catch(exception & e)
+	  {
+		throw runtime_error(theName + ": " + e.what());
+	  }
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Require the double value of the given variable
+   *
+   * Throws if the variable is not set or if the value is not an integer.
+   *
+   * \param theName The variable name
+   * \return The integer value
+   */
+  // ----------------------------------------------------------------------
+  
+  double require_double(const std::string & theName)
+  {
+	const string value = require(theName);
+	try
+	  {
+		double val = lexical_cast<double>(value);
+		return val;
+	  }
+	catch(exception & e)
+	  {
+		throw runtime_error(theName + ": " + e.what());
+	  }
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Require the hour value of the given variable
+   *
+   * Throws if the variable is not set or if the value is not 0-23.
+   *
+   * \param theName The variable name
+   * \return The integer value
+   */
+  // ----------------------------------------------------------------------
+  
+  int require_hour(const std::string & theName)
+  {
+	const int value = require_int(theName);
+	if(value>=0 && value<24)
+	  return value;
+	throw runtime_error(theName+": value "+lexical_cast<string>(value)+" is not 0-23");
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Require the days value of the given variable
+   *
+   * Throws if the variable is not set or if the value is not >= 0.
+   *
+   * \param theName The variable name
+   * \return The integer value
+   */
+  // ----------------------------------------------------------------------
+  
+  int require_days(const std::string & theName)
+  {
+	const int value = require_int(theName);
+	if(value>=0)
+	  return value;
+	throw runtime_error(theName+": value "+lexical_cast<string>(value)+" is not >= 0");
+  }
+  
+}
+
+// ======================================================================
+
