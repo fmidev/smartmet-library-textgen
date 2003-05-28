3546f7948c39971e5e2deab19d5024dbef3dc4a9
mheiskan <mheiskan>
2003-05-28 05:23:29 +0000

    WeatherDataType enumeroi eri datatyypit analysaattoreita varten. Tyypit ovat Forecast, Observation ja Climatology.

---
+++ include/WeatherDataType.h
@@ -0,0 +1,25 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Definition of enumeration WeatherAnalysis::WeatherDataType
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_WEATHERDATATYPE_H
+#define WEATHERANALYSIS_WEATHERDATATYPE_H
+
+namespace WeatherAnalysis
+{
+
+  enum WeatherDataType
+	{
+	  Forecast,
+	  Observation,
+	  Climatology
+	};
+
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_WEATHERDATATYPE_H
+
+// ======================================================================
