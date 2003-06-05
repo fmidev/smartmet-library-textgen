acaf91116267603a3145396554f04b04353f852f
mheiskan <mheiskan>
2003-06-05 16:19:02 +0000

    WeatherPeriodGenerator on abstrakti interface generator-tyyppiselle luokalle, jolta voidaan kysya generoitavien objektien lukumäärä, ja pyytää tietyn indeksin objektia. Tässä objektit ovat WeatherPeriod objekteja, eli ideana on iteroida esim. jonkin ajanjakson öitä tai päiviä.

---
+++ include/WeatherPeriodGenerator.h
@@ -0,0 +1,51 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief An abstract interface for a period generator
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::WeatherPeriodGenerator
+ *
+ * \brief Generates a sequence of periods
+ *
+ * Expected usage is something like
+ * \code
+ * WeatherPeriodGenerator * generator = ...;
+ *
+ * for(WeatherPeriodGenerator::size_type i=1; i<generator->size(); i++)
+ * {
+ *   WeatherPeriod period = generator->period(i);
+ *   ...
+ * }
+ * \endcode
+ */
+// ----------------------------------------------------------------------
+
+#ifndef WEATHERANALYSIS_WEATHERPERIODGENERATOR_H
+#define WEATHERANALYSIS_WEATHERPERIODGENERATOR_H
+
+namespace WeatherAnalysis
+{
+  class WeatherPeriod;
+
+  class WeatherPeriodGenerator
+  {
+  public:
+	typedef unsigned int size_type;
+
+	WeatherPeriodGenerator() { }
+
+	virtual ~WeatherPeriodGenerator() { }
+	virtual size_type size() const;
+	virtual WeatherPeriod period(size_type thePeriod) const;
+
+
+  }; // class WeatherPeriodGenerator
+
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_WEATHERPERIODGENERATOR_H
+
+// ======================================================================
+
