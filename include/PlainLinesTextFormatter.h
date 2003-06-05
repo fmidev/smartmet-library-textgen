a55e546d25c23013742d4b59dd60e1ded918e2f2
mheiskan <mheiskan>
2003-06-05 17:03:12 +0000

    PlainLinesTextFormatter toimii kuten PlainTextFormatter, mutta kappaleita yhdistettäessä se laittaa jokaisen komponentin (käytännössä virkkeen) väliin newlinen. Tällä korvataan vanha pakotettu break- tarina.

---
+++ include/PlainLinesTextFormatter.h
@@ -0,0 +1,34 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class PlainLinesTextFormatter
+ */
+// ======================================================================
+/*!
+ * \class TextGen::PlainLinesTextFormatter
+ *
+ * \brief Glyph visitor generating normal ASCII output
+ */
+// ======================================================================
+
+#ifndef TEXTGEN_PLAINLINESTEXTFORMATTER_H
+#define TEXTGEN_PLAINLINESTEXTFORMATTER_H
+
+#include "PlainTextFormatter.h"
+
+namespace TextGen
+{
+  class PlainLinesTextFormatter : public PlainTextFormatter
+  {
+  public:
+	PlainLinesTextFormatter() { }
+
+	virtual ~PlainLinesTextFormatter() { }
+	virtual std::string visit(const Paragraph & theParagraph) const;
+
+  }; // class PlainLinesTextFormatter
+} // namespace TextGen
+
+#endif // TEXTGEN_PLAINLINESTEXTFORMATTER_H
+
+// ======================================================================
