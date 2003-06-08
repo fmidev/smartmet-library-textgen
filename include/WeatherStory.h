15b0cc51a67493f7f3c6c2622f934341a41f8361
mheiskan <mheiskan>
2003-06-08 15:29:16 +0000

    Lisäsin uuden WeatherStory luokan regressiotesteineen. Luokka ei osaa vielä kertoa yhtään tarinaa, joten regressiotestitkin ovat vielä tyhjiä. Jatkossa luokka kertoo tarinoita yleissääparametreista, kuten sade ja pilvisyys.

---
+++ include/WeatherStory.h
@@ -0,0 +1,64 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class WeatherStory
+ */
+// ======================================================================
+/*!
+ * \class TextGen::WeatherStory
+ *
+ * \brief Generates stories on weather
+ *
+ * \see page_tarinat
+ */
+// ======================================================================
+
+#ifndef TEXTGEN_WEATHERSTORY_H
+#define TEXTGEN_WEATHERSTORY_H
+
+#include "AnalysisSources.h"
+#include "Story.h"
+#include "WeatherArea.h"
+#include "WeatherPeriod.h"
+
+#include <string>
+
+class NFmiTime;
+
+namespace TextGen
+{
+  class Paragraph;
+
+  class WeatherStory : public Story
+  {
+
+  public:
+
+	virtual ~WeatherStory();
+	WeatherStory(const NFmiTime & theForecastTime,
+					 const WeatherAnalysis::AnalysisSources & theSources,
+					 const WeatherAnalysis::WeatherArea & theArea,
+					 const WeatherAnalysis::WeatherPeriod & thePeriod,
+					 const std::string & theVariable);
+
+	static bool hasStory(const std::string & theName);
+	virtual Paragraph makeStory(const std::string & theName) const;
+
+  private:
+
+	WeatherStory();
+	WeatherStory(const WeatherStory & theStory);
+	WeatherStory & operator=(const WeatherStory & theStory);
+
+	const NFmiTime itsForecastTime;
+	const WeatherAnalysis::AnalysisSources & itsSources;
+	const WeatherAnalysis::WeatherArea & itsArea;
+	const WeatherAnalysis::WeatherPeriod & itsPeriod;
+	const std::string itsVar;
+
+  }; // class WeatherStory
+}
+
+#endif // TEXTGEN_WEATHERSTORY_H
+
+// ======================================================================
---
+++ source/WeatherStory.cpp
@@ -0,0 +1,86 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class WeatherStory
+ */
+// ======================================================================
+
+#include "WeatherStory.h"
+#include "Paragraph.h"
+#include "TextGenError.h"
+
+using namespace WeatherAnalysis;
+using namespace boost;
+using namespace std;
+
+namespace TextGen
+{
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Destructor
+   */
+  // ----------------------------------------------------------------------
+  
+  WeatherStory::~WeatherStory()
+  {
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Constructor
+   *
+   * \param theForecastTime The forecast time
+   * \param theSources The analysis sources
+   * \param theArea The area to be analyzed 
+   * \param thePeriod The period to be analyzed
+   * \param theVariable The associated configuration variable
+  */
+  // ----------------------------------------------------------------------
+  
+  WeatherStory::WeatherStory(const NFmiTime & theForecastTime,
+									 const AnalysisSources & theSources,
+									 const WeatherArea & theArea,
+									 const WeatherPeriod & thePeriod,
+									 const string & theVariable)
+	: itsForecastTime(theForecastTime)
+	, itsSources(theSources)
+	, itsArea(theArea)
+	, itsPeriod(thePeriod)
+	, itsVar(theVariable)
+  {
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test whether the given story is known to this class
+   *
+   * \param theName The story name
+   * \return True if this class can generate the story
+   */
+  // ----------------------------------------------------------------------
+  
+  bool WeatherStory::hasStory(const string & theName)
+  {
+	return false;
+  }
+  
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Generate the desired story
+   *
+   * Throws if the story name is not recognized.
+   *
+   * \param theName The story name
+   * \return Paragraph containing the story
+   */
+  // ----------------------------------------------------------------------.
+  
+  Paragraph WeatherStory::makeStory(const string & theName) const
+  {
+	throw TextGenError("WeatherStory cannot make story "+theName);
+
+  }
+
+} // namespace TextGen
+  
+// ======================================================================
