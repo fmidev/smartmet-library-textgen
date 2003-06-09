924fcf493a4c6c4e42ce02b2c102e5c953bc87f8
mheiskan <mheiskan>
2003-06-09 05:50:38 +0000

    Lisäsin CloudinessAnalyzer luokan, jolla tutkitaan käytännössä TotalCloudCover parametria.

---
+++ include/CloudinessAnalyzer.h
@@ -0,0 +1,47 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class WeatherAnalysis::CloudinessAnalyzer
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::CloudinessAnalyzer
+ *
+ * \brief Cloudiness analysis functions
+ *
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYZER_CLOUDINESSANALYZER_H
+#define WEATHERANALYZER_CLOUDINESSANALYZER_H
+
+#include "ParameterAnalyzer.h"
+
+namespace WeatherAnalysis
+{
+  class AnalyzerSources;
+  class WeatherArea;
+  class WeatherPeriodGenerator;
+  class WeatherResult;
+
+  class CloudinessAnalyzer : public ParameterAnalyzer
+  {
+  public:
+
+	virtual WeatherResult analyze(const AnalysisSources & theSources,
+								  const WeatherDataType & theDataType,
+								  const WeatherFunction & theAreaFunction,
+								  const WeatherFunction & theTimeFunction,
+								  const WeatherFunction & theSubTimeFunction,
+								  const WeatherArea & theArea,
+								  const WeatherPeriodGenerator & thePeriods,
+								  const Acceptor & theAreaAcceptor,
+								  const Acceptor & theTimeAcceptor,
+								  const Acceptor & theTester = NullAcceptor()) const;
+
+  };
+}
+
+#endif // WEATHERANALYSIS_CLOUDINESSANALYZER_H
+
+// ======================================================================
