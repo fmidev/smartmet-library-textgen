04e70f084e7750b4d07c5c116aa2ee69782901e3
mheiskan <mheiskan>
2003-06-17 05:35:24 +0000

    NumberFactory luokan avulla voi luoda käyttäjän valitsemalla formatterilla numeroita ja numerovälejä. Näin saadaan esim. Soneralle numerot tulostumaan oikein.

---
+++ include/NumberFactory.h
@@ -0,0 +1,34 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of namespace TextGen::NumberFactory
+ */
+// ======================================================================
+/*!
+ * \namespace TextGen::NumberFactory
+ *
+ * \brief Provides Number and NumberRange creation services.
+ *
+ * The purpose of the factory is to decouple the formatting
+ * of numbers from their creation. The desired formatting is
+ * specified by a global Settings variable.
+ *
+ */
+// ======================================================================
+
+#ifndef TEXTGEN_NUMBERFACTORY_H
+#define TEXTGEN_NUMBERFACTORY_H
+
+#include "Glyph.h"
+#include "boost/shared_ptr.hpp"
+
+namespace TextGen
+{
+  boost::shared_ptr<Glyph> create(int theNumber);
+  boost::shared_ptr<Glyph> create(int theLoLimit, int theHiLimit);
+
+} // namespace TextGen
+
+#endif // TEXTGEN_NUMBERFACTORY_H
+
+// ======================================================================
---
+++ source/NumberFactory.cpp
@@ -0,0 +1,89 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of namespace TextGen::NumberFactory
+ */
+// ======================================================================
+
+#include "NumberFactory.h"
+#include "Number.h"
+#include "NumberRange.h"
+#include "Settings.h"
+#include "Sentence.h"
+#include "TextGenError.h"
+
+using namespace std;
+using namespace boost;
+
+namespace TextGen
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Create an integer
+   *
+   * \param theNumber The integer
+   * \return The glyph
+   */
+  // ----------------------------------------------------------------------
+
+  boost::shared_ptr<Glyph> create(int theNumber)
+  {
+	const string formatter = Settings::optional_string("textgen::numberformatter","default");
+
+	if(formatter == "default")
+	  return shared_ptr<Number<int> >(new Number<int>(theNumber));
+	if(formatter == "sonera")
+	  {
+		shared_ptr<Sentence> s(new Sentence);
+		if(theNumber < -100 || theNumber > 100)
+		  throw TextGenError("Numberformatter 'sonera' supports only numbers -100...100");
+		if(theNumber < 0)
+		  *s << "miinus" << -theNumber;
+		else
+		  *s << theNumber;
+		return s;
+	  }
+
+	throw TextGenError("Numberformatter '"+formatter+"' is unknown");
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Create an integer range
+   *
+   * \param theLoLimit The lower limit
+   * \param theHiLimit The upper limit
+   * \return The glyph
+   */
+  // ----------------------------------------------------------------------
+
+  boost::shared_ptr<Glyph> create(int theLoLimit, int theHiLimit)
+  {
+	if(theLoLimit == theHiLimit)
+	  return create(theLoLimit);
+
+	const string formatter = Settings::optional_string("textgen::numberformatter","default");
+
+	if(formatter == "default")
+	  {
+		typedef NumberRange<Number<int> > IntRange;
+		return shared_ptr<IntRange>(new IntRange(theLoLimit,theHiLimit));
+	  }
+
+	if(formatter == "sonera")
+	  {
+		shared_ptr<Sentence> s(new Sentence);
+		*s << *create(theLoLimit)
+		   << "viiva"
+		   << *create(theHiLimit);
+		return s;
+	  }
+
+	throw TextGenError("Numberformatter '"+formatter+"' is unknown");
+  }
+
+
+} // namespace TextGen
+
+// ======================================================================
