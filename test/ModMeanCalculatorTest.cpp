68dd629296201a2d88a49230a4dda4dfd185d2e3
mheiskan <mheiskan>
2003-07-14 09:47:10 +0000

    Lisäsin regressiotestit ModMeanCalculator luokalle.

---
+++ test/ModMeanCalculatorTest.cpp
@@ -0,0 +1,84 @@
+#include "tframe.h"
+#include "ModMeanCalculator.h"
+#include "NFmiGlobals.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace boost;
+
+
+namespace ModMeanCalculatorTest
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test ModMeanCalculator
+   */
+  // ----------------------------------------------------------------------
+
+  void modmean()
+  {
+	using namespace WeatherAnalysis;
+
+	ModMeanCalculator calc(10);
+
+	if(calc() != kFloatMissing)
+	  TEST_FAILED("Should return kFloatMissing after 0 inserts");
+
+	calc(1);
+	if(calc() != 1)
+	  TEST_FAILED("Failed mean(1) = 1");
+
+	calc(9);
+	if(calc() != (1+-1)/2.0)
+	  TEST_FAILED("Failed mean(1,9) = 0");
+
+	calc(7);
+	if(calc() != 9)
+	  TEST_FAILED("Failed mean(1,9,7) = 9");
+
+	calc(3);
+	if(calc() != 0)
+	  TEST_FAILED("Failed mean(1,9,7,3) = 0");
+
+	TEST_PASSED();
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief The actual test driver
+   */
+  // ----------------------------------------------------------------------
+
+  class tests : public tframe::tests
+  {
+	//! Overridden message separator
+	virtual const char * const error_message_prefix() const
+	{
+	  return "\n\t";
+	}
+
+	//! Main test suite
+	void test(void)
+	{
+	  TEST(modmean);
+	}
+
+  }; // class tests
+
+} // namespace ModMeanCalculatorTest
+
+
+int main(void)
+{
+  using namespace ModMeanCalculatorTest;
+
+  cout << endl
+	   << "ModMeanCalculator tests" << endl
+	   << "=======================" << endl;
+
+  tests t;
+  return t.run();
+}
