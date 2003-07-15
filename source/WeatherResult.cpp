4e47ec4c8f33a4d898710404b16cec8010f994fc
mheiskan <mheiskan>
2003-07-15 05:14:55 +0000

    Korjasin bugin assertissa, piti olla itsAccuracy, ei theAccuracy joka on väärää tyyppiä.

--- source/WeatherResult.cpp
+++ source/WeatherResult.cpp
@@ -29,7 +29,7 @@ namespace WeatherAnalysis
 	: itsValue(theValue)
 	, itsAccuracy(theAccuracy)
   {
-	assert(theAccuracy>=0);
+	assert(itsAccuracy>=0);
   }
 
   // ----------------------------------------------------------------------
@@ -49,7 +49,7 @@ namespace WeatherAnalysis
 	: itsValue(theValue.value())
 	, itsAccuracy(theAccuracy.value())
   {
-	assert(theAccuracy>=0);
+	assert(itsAccuracy>=0);
   }
 
   // ----------------------------------------------------------------------
