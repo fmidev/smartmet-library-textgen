cafe570746b15f24d549f6b7718f5b0143efa23a
mheiskan <mheiskan>
2003-07-15 04:54:39 +0000

    Korjasin Doxygen kommenttien ryhmityksen.

--- include/MessageLoggerStream.h
+++ include/MessageLoggerStream.h
@@ -9,6 +9,17 @@
  *
  * \brief Output stream services for MessageLogger.
  *
+ * Represents a stream that holds a message until it's full,
+ * and then, calls an Event function - onNewMessage
+ *
+ * A message is full when the stream buffer is flushed. 
+ * For instance, when std::endl is written to a stream.
+ *
+ * Usage: Derive your class from this, and implement the PROTECTED
+ * onNewMessage function
+ *
+ * All code copied from CUJ February 2003, code by John Torjo.
+ *
  */
 // ======================================================================
 
@@ -168,22 +179,6 @@ private:
 
 };
 
-
-// ----------------------------------------------------------------------
-/*!
- * Represents a stream that holds a message until it's full,
- * and then, calls an Event function - onNewMessage
- *
- * A message is full when the stream buffer is flushed. 
- * For instance, when std::endl is written to a stream.
- *
- * Usage: Derive your class from this, and implement the PROTECTED
- * onNewMessage function
- *
- * All code copied from CUJ February 2003, code by John Torjo.
- *
- */
-// ----------------------------------------------------------------------
 
 #ifdef UNIX
 template<class char_type ,
