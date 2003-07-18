fb76006886d2374de8ad380084ba008a6fef5203
mheiskan <mheiskan>
2003-07-18 09:22:22 +0000

    Lisäsin regressiotestit MathTools::mean() metodille.

--- test/MathToolsTest.cpp
+++ test/MathToolsTest.cpp
@@ -36,6 +36,33 @@ namespace MathToolsTest
 
   }
 
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test MathTools::mean()
+   */
+  // ----------------------------------------------------------------------
+
+  void mean()
+  {
+	if(MathTools::mean(0,1) != 0.5)
+	  TEST_FAILED("mean(0,1) failed to return 0.5");
+	if(MathTools::mean(2,4) != 3)
+	  TEST_FAILED("mean(2,4) failed to return 3");
+
+	if(MathTools::mean(1,2,3) != 2)
+	  TEST_FAILED("mean(1,2,3) failed to return 2");
+	if(MathTools::mean(1,3,5) != 3)
+	  TEST_FAILED("mean(1,3,5) failed to return 3");
+
+	if(MathTools::mean(1,2,3,4) != 2.5)
+	  TEST_FAILED("mean(1,2,3,4) failed to return 2.5");
+	if(MathTools::mean(1,2,4,5) != 3)
+	  TEST_FAILED("mean(1,2,4,5) failed to return 3");
+
+	TEST_PASSED();
+
+  }
+
   //! The actual test driver
   class tests : public tframe::tests
   {
@@ -49,6 +76,7 @@ namespace MathToolsTest
 	void test(void)
 	{
 	  TEST(to_precision);
+	  TEST(mean);
 	}
 
   }; // class tests
