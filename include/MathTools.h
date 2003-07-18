fe4416a162f7f3431fbfba4587b26076d7e5390e
mheiskan <mheiskan>
2003-07-18 06:38:57 +0000

    Lisäsin apufunktioita useamman argumentin minimin, maksimin ja keskiarvon laskemiseksi.

--- include/MathTools.h
+++ include/MathTools.h
@@ -17,8 +17,113 @@
 
 namespace MathTools
 {
- 
+
   int to_precision(float theValue, int thePrecision);
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return mean of two arguments
+   */
+  // ----------------------------------------------------------------------
+
+  double mean(double arg1, double arg2)
+  {
+	return (arg1+arg2)/2;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return mean of three arguments
+   */
+  // ----------------------------------------------------------------------
+
+  double mean(double arg1, double arg2, double arg3)
+  {
+	return (arg1+arg2+arg3)/3;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return mean of four arguments
+   */
+  // ----------------------------------------------------------------------
+
+  double mean(double arg1, double arg2, double arg3, double arg4)
+  {
+	return (arg1+arg2+arg3+arg4)/4;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return smallest of two arguments
+   */
+  // ----------------------------------------------------------------------
+
+  template <typename T>
+  const T & min(const T & arg1, const T & arg2)
+  {
+	return (arg1 < arg2 ? arg1 : arg2);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return largest of two arguments
+   */
+  // ----------------------------------------------------------------------
+
+  template <typename T>
+  const T & max(const T & arg1, const T & arg2)
+  {
+	return (arg1 < arg2 ? arg2 : arg1);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return smallest of three arguments
+   */
+  // ----------------------------------------------------------------------
+
+  template <typename T>
+  const T & min(const T & arg1, const T & arg2, const T & arg3)
+  {
+	return min(min(arg1,arg2),arg3);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return largest of three arguments
+   */
+  // ----------------------------------------------------------------------
+
+  template <typename T>
+  const T & max(const T & arg1, const T & arg2, const T & arg3)
+  {
+	return max(max(arg1,arg2),arg3);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return smallest of four arguments
+   */
+  // ----------------------------------------------------------------------
+
+  template <typename T>
+  const T & min(const T & arg1, const T & arg2, const T & arg3, const T & arg4)
+  {
+	return min(min(arg1,arg2),min(arg3,arg4));
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return largest of three arguments
+   */
+  // ----------------------------------------------------------------------
+
+  template <typename T>
+  const T & max(const T & arg1, const T & arg2, const T & arg3, const T & arg4)
+  {
+	return max(max(arg1,arg2),max(arg3,arg4));
+  }
 
 } // namespace MathTools
 
