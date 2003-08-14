b6b9c85472011c61ebe36aba5fc782a6b9755731
mheiskan <mheiskan>
2003-08-14 04:40:10 +0000

    Speksasin API:n findRainTimes funktiolle, joka etsii kaikki sadetunnit datasta. Toteutus puuttuu vielä.

--- include/PrecipitationPeriodTools.h
+++ include/PrecipitationPeriodTools.h
@@ -8,10 +8,24 @@
 #ifndef WEATHERANALYSIS_PRECIPITATIONPERIODTOOLS_H
 #define WEATHERANALYSIS_PRECIPITATIONPERIODTOOLS_H
 
+#include <list>
+#include <string>
+
+class NFmiTime;
+
 namespace WeatherAnalysis
 {
+  class AnalysisSources;
+  class WeatherArea;
+  class WeatherPeriod;
+  
   namespace PrecipitationPeriodTools
   {
+
+	std::list<NFmiTime> findRainTimes(const AnalysisSources & theSources,
+									  const WeatherArea & theArea,
+									  const WeatherPeriod & thePeriod,
+									  const std::string & theVar);
 
   } // namespace PrecipitationPeriodTools
 } // namespace WeatherAnalysis
--- source/PrecipitationPeriodTools.cpp
+++ source/PrecipitationPeriodTools.cpp
@@ -19,10 +19,53 @@
 
 #include "PrecipitationPeriodTools.h"
 
+#include "newbase/NFmiTime.h"
+
+#include "AnalysisSources.h"
+#include "WeatherArea.h"
+#include "WeatherPeriod.h"
+
+using namespace std;
+
 namespace WeatherAnalysis
 {
   namespace PrecipitationPeriodTools
   {
+
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Find all moments when it rains for given area
+	 *
+	 * The variables controlling the algorithm are
+	 * \code
+	 * ::minimum_rain = <0->     (default = 0.1)
+	 * ::minimum_area = <0-100>  (default = 20)
+	 * \endcode
+	 * The first variable is the required rain amount in a single
+	 * point for the point to be considered rainy. Variable
+	 * \c minimum_area is the minimum required areal coverage of
+	 * rainy points for the area to be considered rainy. This
+	 * number should be fairly small.
+	 *
+	 * \param theSources The analysis sources
+	 * \param theArea The relevant area
+	 * \param thePeriod The time interval to be analyzed
+	 * \param theVar The variable controlling the algorithm
+	 * \return Sorted list of rainy times
+	 */
+	// ----------------------------------------------------------------------
+
+	std::list<NFmiTime> findRainTimes(const AnalysisSources & theSources,
+									  const WeatherArea & theArea,
+									  const WeatherPeriod & thePeriod,
+									  const std::string & theVar)
+	{
+	  list<NFmiTime> times;
+	  return times;
+	}
+
+
+
   } // namespace PrecipitationPeriodTools
 } // namespace WeatherAnalysis
 
