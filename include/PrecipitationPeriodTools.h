5de96f8c68a1f1d6f4cd1b95a0835039e23e400d
mheiskan <mheiskan>
2003-08-14 04:29:22 +0000

    Uusi PrecipitationPeriodTools namespace sisältää jatkossa funktioita, joiden tarkoitus on analysoida dataa ja etsiä siitä sadejaksoja. Löydettyjä sadejaksoja voidaan myös yhdistellä tiettyjen kriteerien mukaisesti.

---
+++ include/PrecipitationPeriodTools.h
@@ -0,0 +1,21 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of namespace WeatherAnalysis::PrecipitationPeriodTools
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_PRECIPITATIONPERIODTOOLS_H
+#define WEATHERANALYSIS_PRECIPITATIONPERIODTOOLS_H
+
+namespace WeatherAnalysis
+{
+  namespace PrecipitationPeriodTools
+  {
+
+  } // namespace PrecipitationPeriodTools
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_PRECIPITATIONPERIODTOOLS_H
+
+// ======================================================================
---
+++ source/PrecipitationPeriodTools.cpp
@@ -0,0 +1,29 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of namespace WeatherAnalysis::PrecipitationPeriodTools
+ */
+// ======================================================================
+/*!
+ * \namespace WeatherAnalysis::PrecipitationPeriodTools
+ *
+ * \brief Tools related to finding precipitation time intervals
+ *
+ * The namespace contains tools for analyzing a single time
+ * interval, finding all times when it rains, joining them
+ * into precipitation periods, joining precipitation periods
+ * which can be considered to be the same rain period and so on.
+ *
+ */
+// ======================================================================
+
+#include "PrecipitationPeriodTools.h"
+
+namespace WeatherAnalysis
+{
+  namespace PrecipitationPeriodTools
+  {
+  } // namespace PrecipitationPeriodTools
+} // namespace WeatherAnalysis
+
+// ======================================================================
