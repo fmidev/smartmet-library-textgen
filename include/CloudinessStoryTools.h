a3e8e2f746d3e6beb6ec6cd71303ff04ac560173
mheiskan <mheiskan>
2003-09-24 11:28:22 +0000

    CloudinessStoryTools namespace sisältää cloudiness ja weather tarinoille yhteisiä apufunktiota. Alustavasti namespacessa on vain erilaisten pilvisyyskuvauksien enumeraatio CloudinessType.

---
+++ include/CloudinessStoryTools.h
@@ -0,0 +1,45 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of namespace TextGen::CloudinessStoryTools
+ */
+// ======================================================================
+
+#ifndef TEXTGEN_CLOUDINESSSTORYTOOLS_H
+#define TEXTGEN_CLOUDINESSSTORYTOOLS_H
+
+namespace TextGen
+{
+  namespace CloudinessStoryTools
+  {
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Main cloudiness type classification
+	 *
+	 * Note that some algorithms may use only a subset of these
+	 * different cases.
+	 */
+	// ----------------------------------------------------------------------
+
+	enum CloudinessType
+	  {
+		Cloudy,					//!< pilvistä
+		PartlyCloudy,			//!< puolipilvistä
+		Clear,					//!< selkeää
+		MostlyCloudy,			//!< enimmäkseen pilvistä
+		MostlyPartlyCloudy,		//!< enimmäkseen puolipilvistä
+		MostlyClear,			//!< enimmäkseen selkeää
+		CloudyOrPartlyCloudy,	//!< pilvistä tai puolipilvistä
+		ClearOrPartlyCloudy,	//!< selkeää tai puolipilvistä
+		DecreasingCloudiness,	//!< selkenevää
+		IncreasingCloudiness,	//!< pilvistyvää
+		VariableCloudiness		//!< vaihtelevaa pilvisyyttä
+	  };
+
+
+  } // namespace CloudinessStoryTools
+} // namespace TextGen
+
+#endif // TEXTGEN_CLOUDINESSSTORYTOOLS_H
+
+// ======================================================================
---
+++ source/CloudinessStoryTools.cpp
@@ -0,0 +1,29 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation for namespace TextGen::CloudinessStoryTools
+ */
+// ======================================================================
+/*!
+ * \namespace TextGen::CloudinessStoryTools
+ *
+ * \brief Tools for TextGen::CloudinessStory and similar classes
+ *
+ * This namespace contains utility functions which are not
+ * exclusive for handling cloudiness alone. For example,
+ * TextGen::WeatherStory most likely has use for these
+ * functions.
+ */
+// ======================================================================
+
+#include "CloudinessStoryTools.h"
+
+namespace TextGen
+{
+  namespace CloudinessStoryTools
+  {
+
+  } // namespace CloudinessStoryTools
+} // namespace TextGen
+
+// ======================================================================
