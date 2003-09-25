fe04c638ac04f587830881bffcb70d1c40d467e2
mheiskan <mheiskan>
2003-09-25 05:12:48 +0000

    Lisäsin CloudinessStoryTools namespacelle regressiotestit. Nyt siellä on vain similartype, ja sekin ilmoittaa, ettei testejä ole toteutettu. Toimii kuitenkin muistutuksena.

---
+++ test/CloudinessStoryToolsTest.cpp
@@ -0,0 +1,67 @@
+#include "regression/tframe.h"
+#include "Dictionary.h"
+#include "DictionaryFactory.h"
+#include "CloudinessStoryTools.h"
+
+#include "NFmiSettings.h"
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+using namespace boost;
+
+namespace CloudinessStoryToolsTest
+{
+  shared_ptr<TextGen::Dictionary> dict;
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test CloudinessStoryTools::similartype
+   */
+  // ----------------------------------------------------------------------
+
+  void similartype()
+  {
+	TEST_NOT_IMPLEMENTED();
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief The actual test driver
+   */
+  // ----------------------------------------------------------------------
+
+  class tests : public tframe::tests
+  {
+	//! Overridden message separator
+	virtual const char * const error_message_prefix() const
+	{
+	  return "\n\t";
+	}
+
+	//! Main test suite
+	void test(void)
+	{
+	  TEST(similartype);
+	}
+
+  }; // class tests
+
+} // namespace CloudinessStoryToolsTest
+
+
+int main(void)
+{
+  using namespace CloudinessStoryToolsTest;
+
+  cout << endl
+	   << "CloudinessStoryTools tests" << endl
+	   << "==========================" << endl;
+
+  dict = TextGen::DictionaryFactory::create("null");
+
+  tests t;
+  return t.run();
+}
