ac2e7d40b4a2ff48b82bac3eddc06a03bcb4a519
mheiskan <mheiskan>
2003-09-29 09:13:46 +0000

    Erittäin karkeaa toteutusta weather_overview tarinasta, käytännöstä sadepäiville tulee nyt vain erittäin lyhyt viesti. Poutapäivien osuus toimii kohtuullisesti, mutta sitäkin voi parantaa vielä.

--- source/weather_overview.cpp
+++ source/weather_overview.cpp
@@ -6,8 +6,13 @@
 // ======================================================================
 
 #include "WeatherStory.h"
+#include "CloudinessStory.h"
+#include "HourPeriodGenerator.h"
 #include "MessageLogger.h"
 #include "Paragraph.h"
+#include "PrecipitationPeriodTools.h"
+#include "Sentence.h"
+#include "TimeTools.h"
 
 using namespace WeatherAnalysis;
 using namespace std;
@@ -31,7 +36,82 @@ namespace TextGen
   {
 	MessageLogger log("WeatherStory::overview");
 
+	using namespace PrecipitationPeriodTools;
+
 	Paragraph paragraph;
+
+	// we want the last day to extend up to midnight regardless
+	// of the actual period length, otherwise we risk badly
+	// formed forecasts when using full-day descriptions
+
+	WeatherPeriod rainperiod(itsPeriod.localStartTime(),
+							 TimeTools::dayEnd(itsPeriod.localEndTime()));
+
+	RainPeriods rainperiods = analyze(itsSources,
+									  itsArea,
+									  itsPeriod,
+									  itsVar);
+
+	log << "Found " << rainperiods.size() << " rainy periods" << endl;
+
+	if(rainperiods.size() == 0)
+	  {
+		CloudinessStory story(itsForecastTime,
+							  itsSources,
+							  itsArea,
+							  itsPeriod,
+							  itsVar);
+		paragraph << story.makeStory("cloudiness_overview");
+		log << paragraph;
+		return paragraph;
+	  }
+
+	// process each day separately
+
+	HourPeriodGenerator generator(rainperiod, itsVar+"::day");
+	
+	const int n = generator.size();
+	for(int day=1; day<=n; day++)
+	  {
+		WeatherPeriod period = generator.period(day);
+
+		RainPeriods overlap = overlappingPeriods(rainperiods,period);
+		RainPeriods inclusive = inclusivePeriods(rainperiods,period);
+
+		const RainPeriods::size_type noverlap = overlap.size();
+		const RainPeriods::size_type ninclusive = inclusive.size();
+
+		log << "Day " << day << " overlapping rains = " << noverlap << endl;
+		log << "Day " << day << " inclusive rains = " << ninclusive << endl;
+
+		if(noverlap==0 && ninclusive==0)
+		  {
+			CloudinessStory story(itsForecastTime,
+								  itsSources,
+								  itsArea,
+								  period,
+								  itsVar);
+			paragraph << story.makeStory("cloudiness_overview");
+		  }
+		else if(noverlap==0 && ninclusive==1)
+		  {
+			Sentence s;
+			s << "sadetta";
+			paragraph << s;
+		  }
+		else if(noverlap==0 && ninclusive>0)
+		  {
+			Sentence s;
+			s << "ajoittain sateista";
+			paragraph << s;
+		  }
+		else
+		  {
+			Sentence s;
+			s << "sateista";
+			paragraph << s;
+		  }
+	  }
 
 	log << paragraph;
 	return paragraph;
