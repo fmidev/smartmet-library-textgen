13585ec578e19166720d1da6f1dacd78f3ccbb48
mheiskan <mheiskan>
2003-10-01 11:22:10 +0000

    SectionTag of Phrase luokasta johdettu formatointi tagi, jonka tehtävä on ilmoittaa formatterille kyseisen kappaleen nimi, ja siten millä nimellä Settings objektista voi hakea kyseiseen kappaleeseen liittyviä formatointiasetuksia. Toteutettiin, jotta HtmlTextFormatter voisi määritellä konfiguroitavan header tagin headereille, aiemmin tulostettiin aina h1-tagi

---
+++ include/SectionTag.h
@@ -0,0 +1,46 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class TextGen::SectionTag
+ */
+// ======================================================================
+
+#ifndef TEXTGEN_SECTIONTAG_H
+#define TEXTGEN_SECTIONTAG_H
+
+#include "Glyph.h"
+#include <string>
+
+namespace TextGen
+{
+  class Dictionary;
+  class TextFormatter;
+
+  class SectionTag : public Glyph
+  {
+  public:
+
+	virtual ~SectionTag();
+	SectionTag(const std::string & theName);
+#ifdef NO_COMPILER_GENERATED
+	SectionTag(const SectionTag & theSectionTag);
+	SectionTag & operator=(const SectionTag & theSectionTag);
+#endif
+
+	virtual boost::shared_ptr<Glyph> clone() const;
+	virtual std::string realize(const Dictionary & theDictionary) const;
+	virtual std::string realize(const TextFormatter & theFormatter) const;
+
+	virtual bool isDelimiter() const;
+
+  private:
+
+	SectionTag();
+	std::string itsName;
+
+  }; // class SectionTag
+} // namespace SectionTag
+
+#endif // TEXTGEN_SECTIONTAG_H
+
+// ======================================================================
