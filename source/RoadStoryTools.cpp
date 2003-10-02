6d8afb15042c45a0c22707a1fd6b5082896b20ac
mheiskan <mheiskan>
2003-10-02 15:12:09 +0000

    Uusi RoadStoryTools sisältää jatkossa eri RoadStory tarinoiden tarvitsemia apufunktioita.

---
+++ source/RoadStoryTools.cpp
@@ -0,0 +1,27 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of namespace TextGen::RoadStoryTools
+ */
+// ======================================================================
+/*!
+ * \namespace TextGen::RoadStoryTools
+ *
+ * \brief Common utilities for RoadStory methods
+ *
+ */
+// ======================================================================
+
+#include "RoadStoryTools.h"
+
+using namespace std;
+
+namespace TextGen
+{
+  namespace RoadStoryTools
+  {
+
+  } // namespace RoadStoryTools
+} // namespace TextGen
+
+// ======================================================================
