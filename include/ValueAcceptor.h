a390728cebdff6bf9206a84aaf2772bb0cb7c465
mheiskan <mheiskan>
2003-10-08 19:19:37 +0000

    ValueAcceptor palauttaa true-arvon vain yhdelle ainoalle luvulle. Tätä voi käyttää erityisesti laskettaessa enumeroituvan datatyypin prosenttiosuutta, esim. kuinka monta prosenttia sateesta on lunta.

---
+++ include/ValueAcceptor.h
@@ -0,0 +1,35 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class WeatherAnalysis::ValueAcceptor
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_VALUEACCEPTOR_H
+#define WEATHERANALYSIS_VALUEACCEPTOR_H
+
+#include "Acceptor.h"
+
+namespace WeatherAnalysis
+{
+  class ValueAcceptor : public Acceptor
+  {
+  public:
+
+	virtual ~ValueAcceptor() { }
+	virtual bool accept(float theValue) const;
+	virtual boost::shared_ptr<Acceptor> clone() const;
+
+	ValueAcceptor();
+	void value(float theValue);
+
+  private:
+
+	float itsValue;
+
+  }; // class ValueAcceptor
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_VALUEACCEPTOR_H
+
+// ======================================================================
---
+++ source/ValueAcceptor.cpp
@@ -0,0 +1,78 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class WeatherAnalysis::ValueAcceptor
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::ValueAcceptor
+ *
+ * \brief ValueAcceptor accepts only a single value
+ *
+ * This is typically used to calculate the percentage of some
+ * enumerated type out of all types.
+ */
+// ======================================================================
+
+#include "ValueAcceptor.h"
+#include "NFmiGlobals.h"
+
+namespace WeatherAnalysis
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Constructor
+   */
+  // ----------------------------------------------------------------------
+
+  ValueAcceptor::ValueAcceptor()
+	: itsValue(kFloatMissing)
+  {
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Clone
+   */
+  // ----------------------------------------------------------------------
+
+  boost::shared_ptr<Acceptor> ValueAcceptor::clone() const
+  {
+	return boost::shared_ptr<Acceptor>(new ValueAcceptor(*this));
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Accept or reject a value
+   *
+   * The value kFloatMissing is never accepted.
+
+   * \param theValue The value to be accepted
+   * \return True if the value is accepted
+   */
+  // ----------------------------------------------------------------------
+
+  bool ValueAcceptor::accept(float theValue) const
+  {
+	if(theValue == kFloatMissing)
+	  return false;
+	return (theValue == itsValue);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Set the accepted value
+   *
+   * \param theValue The value
+   */
+  // ----------------------------------------------------------------------
+
+  void ValueAcceptor::value(float theValue)
+  {
+	itsValue = theValue;
+  }
+
+} // namespace WeatherAnalysis
+
+// ======================================================================
