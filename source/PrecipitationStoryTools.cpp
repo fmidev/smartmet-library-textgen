f2065658c358ebd1c6b17d665fcd8c77c209103e
mheiskan <mheiskan>
2003-10-08 19:22:48 +0000

    Lisäsin type_phrase funktion, joka palauttaa yleisen kuvauksen sateen voimakkuudesta, olomuodosta ja tyypistä. Näin voimakkaasti koplattu funktio on tarpeen, koska:
       a) Voimakkuusfraasi riippuu olomuodosta
       b) Tallennettuja prompteja on niin vähän, että mielivaltaiset yhdistelmät eivät onnistu
    
    Funktio on vielä olennaisilta osin testaamatta, koska tällä hetkellä ei ole ennustettu
    lumisadetta Suomen alueelle. Testaaminen vanhalla datalla vaatii hieman etsintä
    ja järjestelyaikaa.

--- source/PrecipitationStoryTools.cpp
+++ source/PrecipitationStoryTools.cpp
@@ -24,6 +24,7 @@
 #include "Settings.h"
 #include "Sentence.h"
 #include "TextGenError.h"
+#include "ValueAcceptor.h"
 #include "WeatherResult.h"
 
 #include "NFmiGlobals.h"
@@ -36,6 +37,40 @@ using namespace std;
 
 namespace TextGen
 {
+  namespace
+  {
+
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Possible combinations of rain types
+	 */
+	// ----------------------------------------------------------------------
+
+	enum RainType
+	  {
+		NONE,
+		WATER,
+		SLEET,
+		WATER_SLEET,
+		SNOW,
+		SNOW_WATER,
+		SNOW_SLEET,
+		SNOW_SLEET_WATER
+	  };
+
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Convert booleans to rain type
+	 */
+	// ----------------------------------------------------------------------
+
+	RainType raintype(bool water, bool sleet, bool snow)
+	{
+	  return RainType(snow<<3 | sleet<<2 | water);
+	}
+
+  }
+
   namespace PrecipitationStoryTools
   {
 
@@ -60,8 +95,7 @@ namespace TextGen
 	{
 	  Sentence s;
 	  s << places_phrase(theSources,theArea,thePeriod,theVar,theDay)
-		<< strength_phrase(theSources,theArea,thePeriod,theVar,theDay)
-		<< "sadetta";
+		<< type_phrase(theSources,theArea,thePeriod,theVar,theDay);
 	  return s;
 	}
 
@@ -136,6 +170,249 @@ namespace TextGen
 	  
 	  return s;
 	
+	}
+
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Generate the rain type phrase, including strength
+	 *
+	 * The variables used for determining the strength of the
+	 * precipitation are
+	 * \code
+	 * ::weak_limit = [0-X] (=0.3)
+	 * ::hard_limit = [0-X] (=3.0)
+	 * \endcode
+	 *
+	 * The variables used for determining the rain form are
+	 * \code
+	 * ::ignore_limit  = [0-100] (=5)
+	 * \endcode
+	 *
+	 * The variables used for determining the rain type are
+	 * \code
+	 * ::shower_limit = [0-100] (=80)
+	 * \endcode
+	 */
+	// ----------------------------------------------------------------------
+
+	Sentence type_phrase(const AnalysisSources & theSources,
+						 const WeatherArea & theArea,
+						 const WeatherPeriod & thePeriod,
+						 const string & theVar,
+						 int theDay)
+	{
+	  MessageLogger log("PrecipitationStoryTools::type_phrase");
+
+	  using namespace Settings;
+	 
+	  const double weak_limit = optional_double(theVar+"::weak_limit",0.3);
+	  const double hard_limit = optional_double(theVar+"::hard_limit",3.0);
+	  const int ignore_limit = optional_percentage(theVar+"::ignore_limit",5);
+	  const int shower_limit = optional_percentage(theVar+"::shower_limit",80);
+
+	  GridForecaster forecaster;
+	  Sentence sentence;
+	  
+	  const string day = lexical_cast<string>(theDay);
+
+	  RangeAcceptor waterfilter;
+	  waterfilter.lowerLimit(0);	// 0 = drizzle
+	  waterfilter.upperLimit(1);	// 1 = water
+
+	  WeatherResult water = forecaster.analyze(theVar+"::fake::day"+day+"::water",
+											   theSources,
+											   PrecipitationForm,
+											   Mean,
+											   Percentage,
+											   theArea,
+											   thePeriod,
+											   DefaultAcceptor(),
+											   DefaultAcceptor(),
+											   waterfilter);
+
+	  ValueAcceptor sleetfilter;
+	  sleetfilter.value(2);	// 2 = sleet
+	  WeatherResult sleet = forecaster.analyze(theVar+"::fake::day"+day+"::sleet",
+											   theSources,
+											   PrecipitationForm,
+											   Mean,
+											   Percentage,
+											   theArea,
+											   thePeriod,
+											   DefaultAcceptor(),
+											   DefaultAcceptor(),
+											   sleetfilter);
+
+	  ValueAcceptor snowfilter;
+	  snowfilter.value(3);	// 3 = snow
+	  WeatherResult snow = forecaster.analyze(theVar+"::fake::day"+day+"::snow",
+											   theSources,
+											   PrecipitationForm,
+											   Mean,
+											   Percentage,
+											   theArea,
+											   thePeriod,
+											   DefaultAcceptor(),
+											   DefaultAcceptor(),
+											   snowfilter);
+	  
+	  log << "Water percentage: " << water << endl;
+	  log << "Sleet percentage: " << sleet << endl;
+	  log << "Snow percentage:  " << snow << endl;
+
+	  ValueAcceptor showerfilter;
+	  showerfilter.value(2);	// 1=large scale, 2=showers
+	  WeatherResult showers = forecaster.analyze(theVar+"::fake::day"+day+"::showers",
+												 theSources,
+												 PrecipitationType,
+												 Mean,
+												 Percentage,
+												 theArea,
+												 thePeriod,
+												 DefaultAcceptor(),
+												 DefaultAcceptor(),
+												 showerfilter);
+												 
+	  log << "Shower percentage: " << showers << endl;
+
+	  WeatherResult strength = forecaster.analyze(theVar+"::fake::day"+day+"::strength",
+												  theSources,
+												  Precipitation,
+												  Maximum,
+												  Maximum,
+												  theArea,
+												  thePeriod);
+
+	  log << "Precipitation maximum: " << strength << endl;
+	  
+	  // Safety against weird data. Note that we always assume
+	  // large scale rain, if for some reason we cannot obtain
+	  // the type from the data itself. That is, we never assume
+	  // rain showers.
+
+	  if(water.value() == kFloatMissing ||
+		 sleet.value() == kFloatMissing ||
+		 snow.value() == kFloatMissing ||
+		 strength.value() == kFloatMissing)
+		{
+		  sentence << "sadetta";
+		  return sentence;
+		}
+	  
+	  const bool has_water = (water.value() >= ignore_limit);
+	  const bool has_sleet = (sleet.value() >= ignore_limit);
+	  const bool has_snow  = (snow.value()  >= ignore_limit);
+
+	  const bool has_showers = (showers.value() != kFloatMissing && showers.value() >= shower_limit);
+
+	  switch(raintype(has_water,has_sleet,has_snow))
+		{
+		case NONE:
+		  {
+			sentence << "sadetta";
+			break;
+		  }
+		case WATER:
+		  {
+			if(has_showers)
+			  {
+				if(strength.value() < weak_limit)
+				  sentence << "vähäisiä kuuroja";
+				else if(strength.value() >= hard_limit)
+				  sentence << "ajoittain" << "voimakkaita kuuroja";
+				else
+				  sentence << "sadetta";	// Sopiva prompti puuttuu!!!
+			  }
+			else
+			  {
+				if(strength.value() < weak_limit)
+				  sentence << "heikkoa sadetta";
+				else if(strength.value() >= hard_limit)
+				  sentence << "ajoittain" << "runsasta sadetta";
+				else
+				  sentence << "sadetta";
+			  }
+			break;
+		  }
+		case SLEET:
+		  {
+			sentence << "räntäsadetta";
+			break;
+		  }
+		case WATER_SLEET:
+		  {
+			sentence << "räntä-" << "tai" << "vesisadetta";
+			break;
+		  }
+		case SNOW:
+		  {
+			if(has_showers)
+			  {
+				if(strength.value() < weak_limit)
+				  sentence << "lumikuuroja";	// Sopiva prompti puuttuu!
+				else if(strength.value() >= hard_limit)
+				  sentence << "sakeita lumikuuroja";
+				else
+				  sentence << "lumikuuroja";
+			  }
+			else
+			  {
+				if(strength.value() < weak_limit)
+				  sentence << "vähäistä lumisadetta";
+				else if(strength.value() >= hard_limit)
+				  sentence << "ajoittain" << "sakeaa lumisadetta";
+				else
+				  sentence << "lumisadetta";
+			  }
+			break;
+		  }
+		case SNOW_WATER:
+		case SNOW_SLEET_WATER:
+		  {
+			if(has_showers)
+			  {
+				if(strength.value() < weak_limit)
+				  sentence << "vesi-" << "tai" << "lumikuuroja";
+				else if(strength.value() >= hard_limit)
+				  sentence << "voimakkaita kuuroja" << "tai" << "sakeita lumikuuroja";
+				else
+				  sentence << "vesi-" << "tai" << "lumikuuroja";
+			  }
+			else
+			  {
+				if(strength.value() < weak_limit)
+				  sentence << "heikkoa";
+				else if(strength.value() >= hard_limit)
+				  sentence << "ajoittain" << "kovaa";
+				sentence << "vesi-" << "tai" << "lumisadetta";
+			  }
+			break;
+		  }
+		case SNOW_SLEET:
+		  {
+			if(has_showers)
+			  {
+				if(strength.value() < weak_limit)
+				  sentence << "räntä-" << "tai" << "lumikuuroja";
+				else if(strength.value() >= hard_limit)
+				  sentence << "sakeita lumikuuroja" << "tai" << "räntäsadetta";
+				else
+				  sentence << "räntä-" << "tai" << "lumikuuroja";
+			  }
+			else
+			  {
+				if(strength.value() < weak_limit)
+				  sentence << "vähäistä lumisadetta" << "tai" << "räntäsadetta";
+				else if(strength.value() >= hard_limit)
+				  sentence << "sakeaa lumisadetta" << "tai" << "räntäsadetta";
+				else
+				  sentence << "räntä-" << "tai" << "lumisadetta";
+			  }
+			break;
+		  }
+		}
+
+	  return sentence;
 	}
 
 	// ----------------------------------------------------------------------
