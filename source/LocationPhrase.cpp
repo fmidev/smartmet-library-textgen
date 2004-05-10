243ff72b1b91942dd94df13b3c19f2707e6c728c
mheiskan <mheiskan>
2004-05-10 11:48:47 +0000

    Uusi LocationPhrase tarvitaan, koska kaikkien paikkakuntien nimiä ei haluta syöttää tietokantaan. Toisaalta paikkakuntien nimet täytyy kapitalisoida oikein, joten pelkkä tavallinen LiteralPhrase luokka ei olisi käynyt.

---
+++ source/LocationPhrase.cpp
@@ -0,0 +1,117 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class TextGen::LocationPhrase
+ */
+// ======================================================================
+/*!
+ * \class TextGen::LocationPhrase
+ * \brief Representation of a location name
+ */
+// ======================================================================
+
+#include "LocationPhrase.h"
+#include "Dictionary.h"
+#include "TextFormatter.h"
+#include "TextFormatterTools.h"
+#include "NFmiStringTools.h"
+
+using namespace std;
+using namespace boost;
+
+namespace TextGen
+{
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Destructor
+   */
+  // ----------------------------------------------------------------------
+
+  LocationPhrase::~LocationPhrase()
+  {
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Constructor
+   *
+   * \param theLocation The location name
+   */
+  // ----------------------------------------------------------------------
+
+  LocationPhrase::LocationPhrase(const std::string & theLocation)
+	: itsLocation(theLocation)
+  {
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return a clone
+   */
+  // ----------------------------------------------------------------------
+
+  shared_ptr<Glyph> LocationPhrase::clone() const
+  {
+	shared_ptr<Glyph> ret(new LocationPhrase(*this));
+	return ret;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Realize using the given dictionary
+   *
+   * If there is no translation, the location name is returned back
+   * properly capitalized. That includes capitalizing each word
+   * in the location name separated either by spaces or by hyphens.
+   *
+   * \param theDictionary The dictionary to realize with
+   * \return The realized string
+   */
+  // ----------------------------------------------------------------------
+
+  const std::string LocationPhrase::realize(const Dictionary & theDictionary) const
+  {
+	if(theDictionary.contains(itsLocation))
+	  return theDictionary.find(itsLocation);
+
+	// Capitalize space-separated words
+	vector<string> words = NFmiStringTools::Split(itsLocation," ");
+	for_each(words.begin(),words.end(),TextFormatterTools::capitalize);
+	string name = NFmiStringTools::Join(words," ");
+
+	// Capitalize hyphen-separated words
+	words = NFmiStringTools::Split(name,"-");
+	for_each(words.begin(),words.end(),TextFormatterTools::capitalize);
+	name = NFmiStringTools::Join(words,"-");
+
+	return name;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Realize using the given text formatter
+   *
+   * \param theFormatter The formatter
+   * \return The formatter string
+   */
+  // ----------------------------------------------------------------------
+
+  const std::string LocationPhrase::realize(const TextFormatter & theFormatter) const
+  {
+	return theFormatter.visit(*this);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Returns false since location phrase is not a separator
+   */
+  // ----------------------------------------------------------------------
+
+  bool LocationPhrase::isDelimiter() const
+  {
+	return false;
+  }
+
+} // namespace TextGen
+
+// ======================================================================
