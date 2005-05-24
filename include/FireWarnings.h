e4e005dbfd21b87333e4915e4080d4e23dbfd1da
mheiskan <mheiskan>
2005-05-24 11:47:59 +0000

    Lisäsin luokan FireWarnings, joka osaa lukea peruspalvelulta tulevia palovaroitustiedostoja, ja palauttaa varoitustyypin annetulle aluenumerolle.

---
+++ include/FireWarnings.h
@@ -0,0 +1,44 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class WeatherAnalysis::FireWarnings
+ */
+// ----------------------------------------------------------------------
+
+#ifndef WEATHERANALYSIS_FIREWARNINGS_H
+#define WEATHERANALYSIS_FIREWARNINGS_H
+
+#include "NFmiTime.h"
+#include <string>
+#include <vector>
+
+namespace WeatherAnalysis
+{
+  class FireWarnings
+  {
+  public:
+
+	enum State
+	  {
+		None = 0,
+		GrassFireWarning = 1,
+		FireWarning = 2,
+		Undefined = 999
+	  };
+
+	FireWarnings(const std::string & theDirectory,
+				 const NFmiTime & theTime);
+	State state(int theArea) const;
+
+  private:
+
+	FireWarnings();
+	const NFmiTime itsTime;
+	std::vector<State> itsWarnings;
+
+  }; // class FireWarnings
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_FIREWARNINGS_H
+
+// ======================================================================
---
+++ source/FireWarnings.cpp
@@ -0,0 +1,109 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class WeatherAnalysis::FireWarnings
+ */
+// ======================================================================
+
+#include "FireWarnings.h"
+#include "WeatherAnalysisError.h"
+// newbase
+#include "NFmiFileSystem.h"
+#include "NFmiStringTools.h"
+
+using namespace std;
+
+namespace WeatherAnalysis
+{
+
+  //! Firewarning areas run from 1 to 46
+
+  const int MaxAreaCode = 46;
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief FireWarnings constructor
+   */
+  // ----------------------------------------------------------------------
+
+  FireWarnings::FireWarnings(const string & theDirectory,
+							 const NFmiTime & theTime)
+	: itsTime(theTime)
+	, itsWarnings(MaxAreaCode,Undefined)
+  {
+	if(!NFmiFileSystem::DirectoryExists(theDirectory))
+	  throw WeatherAnalysisError("Directory '"+theDirectory+"' required by class FireWarnings does not exist");
+
+	// Form the expected filename of form YYYYMMDD.palot_koodina
+	// If we cannot find the file, an exception is thrown
+
+	string filename = (theDirectory +
+					   '/' +
+					   theTime.ToStr(kYYYYMMDD).CharPtr() +
+					   ".palot_koodina");
+	if(!NFmiFileSystem::FileExists(filename))
+	  {
+		NFmiTime tmp = theTime;
+		tmp.ChangeByDays(-1);
+		filename = (theDirectory +
+					'/' +
+					tmp.ToStr(kYYYYMMDD).CharPtr() +
+					".palot_koodina");
+		if(!NFmiFileSystem::FileExists(filename))
+		  throw WeatherAnalysisError("Cannot find warnings from '"+theDirectory+"'");
+	  }
+
+	// Read the file
+
+	ifstream input(filename.c_str(), ios::in);
+	if(!input)
+	  throw WeatherAnalysisError("Failed to open '"+filename+"' for reading");
+
+	// Skip the date
+	string tmp;
+	input >> tmp;
+
+	// Read the areacode - state pairs
+
+	int areacode;
+	int areastate;
+	while(input >> areacode >> areastate)
+	  {
+		if(areacode < 1 || areacode > MaxAreaCode)
+		  throw WeatherAnalysisError("File '"+filename+"' contains invalid areacode "+NFmiStringTools::Convert(areacode));
+		switch(State(areastate))
+		  {
+		  case None:
+		  case GrassFireWarning:
+		  case FireWarning:
+			itsWarnings[areacode] = State(areastate);
+			break;
+		  default:
+			throw WeatherAnalysisError("File '"+filename+"' contains invalid warningcode "+NFmiStringTools::Convert(areastate));
+		  }
+	  }
+	input.close();
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief get the warning state for the given area code
+   *
+   * \param theArea The area code (1...46)
+   * \return The state
+   */
+  // ----------------------------------------------------------------------
+
+  FireWarnings::State FireWarnings::state(int theArea) const
+  {
+	if(theArea < 1 || theArea > MaxAreaCode)
+	  throw WeatherAnalysisError("Only area codes 1...46 are allowed in FireWarnings");
+	return itsWarnings[theArea];
+  }
+
+  
+
+
+} // namespace WeatherAnalysis
+
+// ======================================================================
