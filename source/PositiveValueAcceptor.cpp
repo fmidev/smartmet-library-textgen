2ba15ea08597ecc5c9ab30338e3e910ed048e11e
reponen <reponen>
2010-02-12 13:05:34 +0000

    Accepts only positive values

---
+++ source/PositiveValueAcceptor.cpp
@@ -0,0 +1,51 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class WeatherAnalysis::PositiveValueAcceptor
+ */
+// ======================================================================
+/*!
+ * \class WeatherAnalysis::PositiveValueAcceptor
+ *
+ * \brief PositiveValueAcceptor accepts only positive values
+ *
+ */
+// ======================================================================
+
+
+#include "PositiveValueAcceptor.h"
+#include <newbase/NFmiGlobals.h>
+
+namespace WeatherAnalysis
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Clone
+   */
+  // ----------------------------------------------------------------------
+
+  boost::shared_ptr<Acceptor> PositiveValueAcceptor::clone() const
+  {
+	return boost::shared_ptr<Acceptor>(new PositiveValueAcceptor(*this));
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Accept or reject a value
+   *
+   * Only positive value is accepted, , but value kFloatMissing is never accepted.
+
+   * \param theValue The value to be accepted
+   * \return True if the value is accepted
+   */
+  // ----------------------------------------------------------------------
+
+  bool PositiveValueAcceptor::accept(float theValue) const
+  {
+	return (theValue != kFloatMissing && theValue > 0.0);
+  }
+
+} // namespace WeatherAnalysis
+
+// ======================================================================
