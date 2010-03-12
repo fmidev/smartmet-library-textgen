46a5c2b773a9eeef8d9a6a374d64e2697ab9fa4e
reponen <reponen>
2010-03-12 07:18:52 +0000

    Acceptor that accepts float values based on given limit and operator

---
+++ include/ComparativeAcceptor.h
@@ -0,0 +1,46 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class WeatherAnalysis::ComparativeAcceptor
+ */
+// ======================================================================
+
+#ifndef WEATHERANALYSIS_COMPARATIVEACCEPTOR_H
+#define WEATHERANALYSIS_COMPARATIVEACCEPTOR_H
+
+#include "Acceptor.h"
+#include <newbase/NFmiGlobals.h>
+
+namespace WeatherAnalysis
+{
+  enum comparative_operator{LESS_THAN, 
+							LESS_OR_EQUAL, 
+							EQUAL, 
+							GREATER_OR_EQUAL, 
+							GREATER_THAN,
+							VOID_OPERATOR};
+
+  class ComparativeAcceptor : public Acceptor
+  {
+  public:
+
+	virtual ~ComparativeAcceptor() { }
+	virtual bool accept(float theValue) const;
+	virtual boost::shared_ptr<Acceptor> clone() const;
+
+	ComparativeAcceptor(const float& theLimit = kFloatMissing,
+						const comparative_operator& theOperator = VOID_OPERATOR);
+	void setLimit(float theLimit);
+	void setOperator(const comparative_operator& theOperator);
+
+  private:
+
+	float itsLimit;
+	comparative_operator itsOperator;
+
+  }; // class ComparativeAcceptor
+} // namespace WeatherAnalysis
+
+#endif // WEATHERANALYSIS_COMPARATIVEACCEPTOR_H
+
+// ======================================================================
