7419e01ad763f968b4e81bf5bdf048802faad4d0
Pekka KerÃ¤nen <pekka.keranen@geosaaga.fi>
2011-03-15 10:35:52 +0200

    Fixed compilation in RHEL6

--- include/WeatherStory.h
+++ include/WeatherStory.h
@@ -46,6 +46,7 @@ namespace TextGen
 	const Paragraph shortoverview() const;
 	const Paragraph thunderprobability() const;
 	const Paragraph thunderprobability_simplified() const;
+	const Paragraph overview2() const;
 
 	WeatherStory();
 	WeatherStory(const WeatherStory & theStory);
