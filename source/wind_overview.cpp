63e28e344a58a4c3f7c16a1e6f6dea444727769f
Anssi Reponen <anssi.reponen@fmi.fi>
2011-08-02 16:43:28 +0300

    calculation of key-parameters done

--- source/wind_overview.cpp
+++ source/wind_overview.cpp
@@ -5,9 +5,49 @@
 #include "WeatherResult.h"
 #include "WindStoryTools.h"
 #include "GridForecaster.h"
+#include "RegularFunctionAnalyzer.h"
+#include "ParameterAnalyzer.h"
+#include "QueryDataIntegrator.h"
+#include "CalculatorFactory.h"
+#include "SumCalculator.h"
+#include "MaximumCalculator.h"
+#include "MinimumCalculator.h"
+#include "MeanCalculator.h"
+#include "PositiveValueAcceptor.h"
+#include <boost/lexical_cast.hpp>
+#include <fstream>
 
+#include "SubMaskExtractor.h"
+#include "MaskSource.h"
+#include "Settings.h"
+#include "WeatherAnalysisError.h"
+#include "WeatherResult.h"
+#include "WeatherSource.h"
+#include "QueryDataTools.h"
+#include "StandardDeviationCalculator.h"
+
+#include <newbase/NFmiStreamQueryData.h>
+#include <newbase/NFmiNearTree.h>
+#include <newbase/NFmiEnumConverter.h>
+#include <newbase/NFmiFastQueryInfo.h>
+#include <newbase/NFmiQueryData.h>
+#include <newbase/NFmiFastQueryInfo.h>
+#include <newbase/NFmiIndexMask.h>
+#include <newbase/NFmiIndexMaskSource.h>
+#include <newbase/NFmiTime.h>
+#include <newbase/NFmiMetMath.h>
+#include <newbase/NFmiSvgTools.h>
+#include <cassert>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/lexical_cast.hpp>
+#include <string>
+#include <sstream>
+
+using namespace boost;
 using namespace WeatherAnalysis;
 using namespace TextGen::WindStoryTools;
+using namespace WeatherAnalysis::QueryDataIntegrator;
 using namespace std;
 
 namespace TextGen
@@ -35,6 +75,48 @@ namespace TextGen
 	WeatherResult theWindDirection;
 	WindDirectionAccuracy theDirectionAccuracy;
   };
+
+  std::ostream& operator<<(std::ostream & theOutput,
+						   const WindDataItem& theWindDataItem)
+  {
+	/*
+	const WeatherResult windspeedMax(theWindDataItem.windspeedMax);
+	const WeatherResult windspeedMin(theWindDataItem.windspeedMin);
+	const WeatherResult windspeedMean(theWindDataItem.windspeedMean);
+	*/
+	theOutput << theWindDataItem.thePeriod.localStartTime()
+			  << " ... "
+			  << theWindDataItem.thePeriod.localEndTime()
+			  << ": ("
+			  << theWindDataItem.theWindspeedMin.value()
+			  << ", "
+			  << theWindDataItem.theWindspeedMin.error()
+			  << ")"
+			  << "("
+			  << theWindDataItem.theWindspeedMax.value()
+			  << ", "
+			  << theWindDataItem.theWindspeedMax.error()
+			  << ")"
+			  << "("
+			  << theWindDataItem.theWindspeedMean.value()
+			  << ", "
+			  << theWindDataItem.theWindspeedMean.error()
+			  << ")"
+			  << endl;
+	/*
+	theOutput << theWindDataItem.period.localStartTime()
+			  << " ... "
+			  << theWindDataItem.period.localEndTime()
+			  << ": "
+			  << theWindDataItem.windspeedMin.value()
+			  << ','
+			  << theWindDataItem.windspeedMin.error()
+			  << ')'
+			  << endl;
+	*/
+
+	return theOutput;
+  }
 
   typedef vector<WindDataItem*> wind_data_item_vector;
 
@@ -129,16 +211,90 @@ void populate_wind_time_series(const string& theVar,
 	}
 }
 
+float calculate_standard_deviation(const WeatherParameter& theParameter,
+								  const WeatherPeriod& thePeriod,
+								  const AnalysisSources& theSources,
+								  const WeatherArea& theArea)
+{
+	std::string parameterName;
+	std::string dataName;
+
+	ParameterAnalyzer::getParameterStrings(theParameter, parameterName, dataName);
+	const string default_forecast = Settings::optional_string("textgen::default_forecast","");
+	const string datavar = dataName + "_forecast";
+	const string dataname = Settings::optional_string(datavar, default_forecast);
+
+	// Get the data into use
+	  
+	shared_ptr<WeatherSource> wsource = theSources.getWeatherSource();
+	shared_ptr<NFmiStreamQueryData> qd = wsource->data(dataname);
+	//	const NFmiGrid* theGrid = qd->QueryInfoIter()->Grid();
+
+	NFmiFastQueryInfo* q = qd->QueryInfoIter();
+	NFmiIndexMask areaMask;
+	PositiveValueAcceptor acceptor;
+	ExtractMask(theSources,
+				WindSpeed,
+				theArea,
+				thePeriod,
+				acceptor,
+				areaMask);
+
+	StandardDeviationCalculator spaceCalc;
+	StandardDeviationCalculator timeCalc;
+
+	float retval = Integrate(*q, 
+					 areaMask, 
+					 spaceCalc,
+					 thePeriod.localStartTime(), 
+					 thePeriod.localEndTime(), 
+					 timeCalc);
+
+	//	cout << spaceCalc() << endl;
+	//	cout << timeCalc() << endl;
+
+	return retval;
+}
+
+
   const Paragraph WindStory::overview() const
   {
 	MessageLogger log("WeatherStory::overview");
+
+	std::string areaName("");
+	if(itsArea.isNamed())
+	  {
+		areaName = itsArea.name();
+		log << "** " << areaName << " **" << endl;
+	  }
 
     // Generate the story
     //
 	Paragraph paragraph;
 
-	wind_data_item_vector resultVector;
-  
+	wind_data_item_vector theResultVector;
+	
+	allocate_data_structures(itsPeriod, 
+							 theResultVector);
+
+
+	populate_wind_time_series(itsVar, 
+							  itsSources,
+							  itsArea,	
+							  theResultVector);
+
+	for(unsigned int i = 0; i < theResultVector.size(); i++)
+	  {
+		log << *(theResultVector[i]);
+	  }
+	log << "standard deviation: " 
+		<< calculate_standard_deviation(WindSpeed,
+										itsPeriod,
+										itsSources,
+										itsArea)
+		<< endl;
+	
+	deallocate_data_structure(theResultVector);
 
 	log << paragraph;
 	return paragraph;
