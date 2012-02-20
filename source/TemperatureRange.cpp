7d866221367a8c694367caeb375820821dbb3fa5
Anssi Reponen <anssi.reponen@fmi.fi>
2012-02-20 09:15:24 +0200

    New class to handle temperature range

---
+++ source/TemperatureRange.cpp
@@ -0,0 +1,87 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class TextGen::TemperatureRange
+ */
+// ======================================================================
+/*!
+ * \class TextGen::TemperatureRange
+ * \brief Representation of a generic TemperatureRange
+ */
+// ======================================================================
+
+#include "TemperatureRange.h"
+#include "Dictionary.h"
+#include "TextGenError.h"
+
+#include <boost/shared_ptr.hpp>
+#include <sstream>
+
+using namespace boost;
+using namespace std;
+
+namespace TextGen
+{
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Constructor
+   */
+  // ----------------------------------------------------------------------
+
+  TemperatureRange::TemperatureRange(int theStartValue,
+									 int theEndValue,
+									 const std::string & theSeparator) 
+	: IntegerRange(theStartValue,
+				   theEndValue,
+				   theSeparator)
+  {
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return a clone
+   */
+  // ----------------------------------------------------------------------
+
+  shared_ptr<Glyph> TemperatureRange::clone() const
+  {
+	shared_ptr<Glyph> ret(new TemperatureRange(*this));
+	return ret;
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return the text for the range
+   *
+   * \param theDictionary The dictionary to be used
+   * \return The text
+   */
+  // ----------------------------------------------------------------------
+
+  const std::string TemperatureRange::realize(const Dictionary & theDictionary) const
+  {
+	std::ostringstream os;
+	if(itsStartValue == itsEndValue)
+	  os << itsStartValue;
+	else
+	  {
+		if(itsStartValue <= 0 && itsEndValue > 0)
+		  os << itsStartValue << itsRangeSeparator << "+" << itsEndValue;
+		else if(itsStartValue > 0 && itsEndValue <= 0)
+		  os << "+" << itsStartValue << itsRangeSeparator << itsEndValue;
+		else
+		  os << itsStartValue << itsRangeSeparator << itsEndValue;
+	  }
+
+	return os.str();
+  }
+
+  const std::string TemperatureRange::realize(const TextFormatter & theFormatter) const
+  {
+	return theFormatter.visit(*this);
+  }
+
+} // namespace TextGen
+
+// ======================================================================
