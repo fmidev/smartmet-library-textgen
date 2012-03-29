ddb3898cc18bd292a00c054421acc5f302406a14
Mika Heiskanen <mika.heiskanen@fmi.fi>
2012-03-29 16:11:07 +0300

    special_text story now works according to specs

---
+++ test/SpecialStoryTest.cpp
@@ -0,0 +1,167 @@
+#include <regression/tframe.h>
+#include "Dictionary.h"
+#include "DictionaryFactory.h"
+#include "Paragraph.h"
+#include "PlainTextFormatter.h"
+#include "SpecialStory.h"
+#include "Story.h"
+#include "MessageLogger.h"
+
+#include <newbase/NFmiSettings.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#include <boost/locale.hpp>
+
+using namespace std;
+using namespace boost;
+
+
+namespace SpecialStoryTest
+{
+  shared_ptr<TextGen::Dictionary> dict;
+  TextGen::PlainTextFormatter formatter;
+  
+
+  void require(const TextGen::Story & theStory,
+			   const string & theLanguage,
+			   const string & theName,
+			   const string & theExpected)
+  {
+	dict->init(theLanguage);
+	formatter.dictionary(dict);
+
+	TextGen::Paragraph para = theStory.makeStory(theName);
+	const string value = para.realize(formatter);
+
+	if(value != theExpected)
+	  {
+		const string msg = "'" + value + "' <> '" + theExpected + "'";
+		TEST_FAILED(msg.c_str());
+	  }
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test SpecialStory::none()
+   */
+  // ----------------------------------------------------------------------
+
+  void special_none()
+  {
+	using namespace std;
+	using namespace TextGen;
+	using namespace WeatherAnalysis;
+
+	AnalysisSources sources;
+	WeatherArea area("25,60");
+	NFmiTime time1(2000,1,1);
+	NFmiTime time2(2000,1,2);
+	WeatherPeriod period(time1,time2);
+	SpecialStory story(time1,sources,area,period,"none");
+
+	const string fun = "none";
+
+	require(story,"fi",fun,"");
+	require(story,"sv",fun,"");
+	require(story,"en",fun,"");
+
+	TEST_PASSED();
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test SpecialStory::text()
+   */
+  // ----------------------------------------------------------------------
+
+  void special_text()
+  {
+	using namespace std;
+	using namespace TextGen;
+	using namespace WeatherAnalysis;
+
+	AnalysisSources sources;
+	WeatherArea area("25,60");
+	NFmiTime time1(2000,1,1);
+	NFmiTime time2(2000,1,2);
+	WeatherPeriod period(time1,time2);
+	SpecialStory story(time1,sources,area,period,"text");
+
+	const string fun = "text";
+
+	NFmiSettings::Set("text::value","This is the value of the string.");
+
+	require(story,"fi",fun,"This is the value of the string.");
+	require(story,"sv",fun,"This is the value of the string.");
+	require(story,"en",fun,"This is the value of the string.");
+
+	NFmiSettings::Set("text::value","@data/special_text.fi");
+	require(story,"fi",fun,"Suomenkielinen teksti.");
+
+	NFmiSettings::Set("text::value","@data/special_text.en");
+	require(story,"fi",fun,"English text.");
+
+	NFmiSettings::Set("text::value","@data/special_text.php");
+	require(story,"fi",fun,"Suomenkieltä PHP-ohjelmasta");
+
+	TEST_PASSED();
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief The actual test driver
+   */
+  // ----------------------------------------------------------------------
+
+  class tests : public tframe::tests
+  {
+	//! Overridden message separator
+	virtual const char * error_message_prefix() const
+	{
+	  return "\n\t";
+	}
+
+	//! Main test suite
+	void test(void)
+	{
+	  TEST(special_none);
+	  TEST(special_text);
+	}
+
+  }; // class tests
+
+} // namespace SpecialStoryTest
+
+
+int main(void)
+{
+  boost::locale::generator generator;
+  std::locale::global(generator(""));
+
+  NFmiSettings::Init();
+  NFmiSettings::Set("textgen::database","textgen2");
+
+  using namespace SpecialStoryTest;
+
+
+  MessageLogger::open("my.log");
+
+
+  cout << endl
+	   << "SpecialStory tests" << endl
+	   << "=================" << endl;
+
+  dict.reset(TextGen::DictionaryFactory::create("multimysql"));
+
+  dict->init("fi");
+  dict->init("sv");
+  dict->init("en");
+
+  NFmiSettings::Set("textgen::specialseason","true");
+
+  tests t;
+  return t.run();
+}
