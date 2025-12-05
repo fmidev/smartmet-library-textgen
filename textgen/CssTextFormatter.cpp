f0cf899377ede5742336eb3534c88236c84a6b12
Mika Heiskanen <mika.heiskanen@fmi.fi>
2025-12-05 11:31:25 +0200

    Fixed syntax errors in variable names

--- textgen/CssTextFormatter.cpp
+++ textgen/CssTextFormatter.cpp
@@ -208,7 +208,7 @@ string CssTextFormatter::visit(const Paragraph& theParagraph) const
 string CssTextFormatter::visit(const Header& theHeader) const
 {
   bool colon = Settings::optional_bool(itsSectionVar + "::header::colon", false);
-  colon = Settings::optional_bool(itsSectionVar + "::header::css:colon", colon);
+  colon = Settings::optional_bool(itsSectionVar + "::header::css::colon", colon);
 
   string text = TextFormatterTools::realize(theHeader.begin(), theHeader.end(), *this, " ", "");
   text = TextFormatterTools::capitalize(text);
